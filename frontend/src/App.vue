3dc39d273082e3e80480db331d3faffbf4d91cae
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2024-12-13 13:37:50 +0100

    Prettier

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import NavbarTop from './components/NavbarTop.vue';
+import NavbarTop from './components/NavbarTop.vue'
 </script>
 
 <template>
--- frontend/src/components/NavbarTop.vue
+++ frontend/src/components/NavbarTop.vue
@@ -1,11 +1,7 @@
-<script lang="ts" setup>
-
-</script>
+<script lang="ts" setup></script>
 
 <template>
-  <nav
-    class="flex items-center justify-between border-y border-gray-400 py-16 pe-16 print:hidden"
-  >
+  <nav class="flex items-center justify-between border-y border-gray-400 py-16 pe-16 print:hidden">
     <div class="flex items-center gap-44">
       <div class="flex items-center">
         <span class="px-[1rem] text-14 font-bold uppercase leading-16">
