75a0652b8c2f18415e215ab7b0a9f169f683c8d4
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2024-12-16 14:45:55 +0100

    Move test next to component

--- frontend/src/components/NavbarTop.spec.ts
+++ frontend/src/components/NavbarTop.spec.ts
@@ -1,7 +1,7 @@
 import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
-import NavbarTop from '../NavbarTop.vue'
+import NavbarTop from './NavbarTop.vue'
 
 describe('HelloWorld', () => {
   it('renders properly', () => {
