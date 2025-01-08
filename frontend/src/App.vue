10f16a2a8c2b861c9a0a6adb12735ca0b31dbd83
andreas-deazevedo <andreas.deazevedo@digitalservice.bund.de>
2025-01-08 14:47:27 +0100

    RISDEV-6041 Fix header layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,5 +7,10 @@ import NavbarTop from './components/NavbarTop.vue'
     <NavbarTop />
     👉 Falls es um Verwaltungsvorschriften (ADM/VwV) geht, sind Sie am richtigen Ort, aber es gibt
     noch nichts weiter zu sehen.
+    <Suspense>
+      <main class="flex grow">
+        <router-view class="grow" />
+      </main>
+    </Suspense>
   </div>
 </template>
