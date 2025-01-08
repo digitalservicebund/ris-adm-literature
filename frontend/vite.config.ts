61eb4efc2b287f2b50b2184513d15db17f832108
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2025-01-08 20:38:37 +0100

    Add icons via unplugin-icons (runs fine, build fails)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,25 +1,34 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { sentryVitePlugin } from '@sentry/vite-plugin'
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import Icons from 'unplugin-icons/vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools(), sentryVitePlugin({
-    org: "digitalservice",
-    project: "ris-adm-vwv"
-  })],
+  plugins: [
+    Icons({
+      /* options */
+    }),
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+    sentryVitePlugin({
+      org: 'digitalservice',
+      project: 'ris-adm-vwv',
+    }),
+  ],
 
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 
   build: {
-    sourcemap: true
-  }
+    sourcemap: true,
+  },
 })
