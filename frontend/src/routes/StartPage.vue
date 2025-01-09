e810e0b7a4d84cfdac191f80c85826952ebbf074
andreas-deazevedo <andreas.deazevedo@digitalservice.bund.de>
2025-01-09 11:25:32 +0100

    RISDEV-6041 Adjust layout

--- frontend/src/routes/StartPage.vue
+++ frontend/src/routes/StartPage.vue
@@ -3,12 +3,14 @@ import TextButton from "@/components/input/TextButton.vue"
 </script>
 
 <template>
-  <div class="flex justify-between">
-    <h1 class="ds-heading-02-reg">Übersicht Verwaltungsvorschriften</h1>
+  <div class="flex flex-col gap-16 p-16">
+    <div class="flex justify-between">
+      <h1 class="ds-heading-02-reg">Übersicht Verwaltungsvorschriften</h1>
 
-    <TextButton
-      label="Neue Dokumentationseinheit"
-    />
+      <TextButton
+        label="Neue Dokumentationseinheit"
+      />
+    </div>
   </div>
 
 </template>
