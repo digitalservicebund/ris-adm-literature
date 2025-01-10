8d2ca2e7d196e76dcd7fa0da05dec5026b16fdcb
Hannes <hannes.michalek@digitalservice.bund.de>
2025-01-10 09:14:12 +0100

    Show ErrorNotFound on unknown routes (#44)

---
+++ frontend/src/routes/ErrorNotFound.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="flex flex-col gap-16 p-16">
+    <div class="flex justify-between">
+      <h1 class="ds-heading-02-reg">Seite nicht gefunden (404 NOT FOUND)</h1>
+    </div>
+  </div>
+</template>
