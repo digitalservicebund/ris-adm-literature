572ff924974379a23470c69195c66f51c84c794a
Sebastian Reck <sebastian.reck@digitalservice.bund.de>
2025-01-13 18:25:06 +0100

    Add abgabe button which leads to startpage

--- frontend/src/routes/documentUnit/[documentNumber]/AbgabePage.vue
+++ frontend/src/routes/documentUnit/[documentNumber]/AbgabePage.vue
@@ -1,5 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+import TextButton from '@/components/input/TextButton.vue'
+
+const router = useRouter()
+</script>
 
 <template>
-  <h1 class="ds-heading-02-reg">Abgabe</h1>
+  <div class="flex w-full flex-1 grow flex-col p-24">
+    <div aria-label="Abgabe" class="flex flex-col gap-24 bg-white p-24">
+      <TitleElement>Abgabe</TitleElement>
+      <div class="flex flex-row">
+        <TextButton
+          label="Abgeben"
+          @click="router.push({ path: '/' })"
+        />
+      </div>
+    </div>
+  </div>
 </template>
