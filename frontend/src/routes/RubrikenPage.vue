9b72ab7a69df56614d21ed6e43ae14fe52b96b98
Sebastian Reck <sebastian.reck@digitalservice.bund.de>
2025-01-13 16:35:21 +0100

    delte commented out code

--- frontend/src/routes/RubrikenPage.vue
+++ frontend/src/routes/RubrikenPage.vue
@@ -1,141 +1,23 @@
 <script lang="ts" setup>
-// import { useHead } from "@unhead/vue"
-// import { storeToRefs } from "pinia"
-// import { onBeforeUnmount, onMounted, ref, Ref } from "vue"
 import { ref, type Ref } from 'vue'
 import { useRoute } from 'vue-router'
 import DocumentUnitInfoPanel from '@/components/DocumentUnitInfoPanel.vue'
-// import ExtraContentSidePanel from "@/components/ExtraContentSidePanel.vue"
-// import FlexContainer from "@/components/FlexContainer.vue"
-// import { ValidationError } from "@/components/input/types"
 import NavbarSide from '@/components/NavbarSide.vue'
-// import ErrorPage from "@/components/PageError.vue"
 import SideToggle from '@/components/SideToggle.vue'
 import { useAdmVwvMenuItems } from '@/composables/useAdmVwvMenuItems'
-// import useQuery from "@/composables/useQueryFromRoute"
-// import DocumentUnit from "@/domain/documentUnit"
-// import { ResponseError } from "@/services/httpClient"
-// import { useDocumentUnitStore } from "@/stores/documentUnitStore"
-// import { useExtraContentSidePanelStore } from "@/stores/extraContentSidePanelStore"
 
-// const props = defineProps<{
-//   documentNumber: string
-// }>()
-
-// useHead({
-//   title: props.documentNumber + " · NeuRIS Rechtsinformationssystem",
-// })
-
-// const store = useDocumentUnitStore()
-// const extraContentSidePanelStore = useExtraContentSidePanelStore()
-
-// const { documentUnit } = storeToRefs(store) as {
-//   documentUnit: Ref<DocumentUnit | undefined>
-// }
 const route = useRoute()
-// const menuItems = useAdmVwvMenuItems(props.documentNumber, route.query)
 const menuItems = useAdmVwvMenuItems('KSNR707', route.query)
-// const { pushQueryToRoute } = useQuery()
 
-// const validationErrors = ref<ValidationError[]>([])
 const showNavigationPanelRef: Ref<boolean> = ref(route.query.showNavigationPanel !== 'false')
-
-// const responseError = ref<ResponseError>()
 
 function toggleNavigationPanel(expand?: boolean) {
   showNavigationPanelRef.value = expand === undefined ? !showNavigationPanelRef.value : expand
-  //   pushQueryToRoute({
-  //     ...route.query,
-  //     showNavigationPanel: showNavigationPanelRef.value.toString(),
-  //   })
 }
-
-// async function requestDocumentUnitFromServer() {
-//   const response = await store.loadDocumentUnit(props.documentNumber)
-
-//   if (!response.data) {
-//     responseError.value = response.error
-//   }
-// }
-
-// async function attachmentIndexSelected(index: number) {
-//   extraContentSidePanelStore.togglePanel(true)
-//   extraContentSidePanelStore.selectAttachments(index)
-// }
-
-// async function attachmentIndexDeleted(index: number) {
-//   await requestDocumentUnitFromServer()
-//   extraContentSidePanelStore.onAttachmentDeleted(
-//     index,
-//     documentUnit.value ? documentUnit.value.attachments.length - 1 : 0,
-//   )
-// }
-
-// async function attachmentsUploaded(anySuccessful: boolean) {
-//   if (anySuccessful) {
-//     await requestDocumentUnitFromServer()
-//     extraContentSidePanelStore.togglePanel(true)
-//     extraContentSidePanelStore.selectAttachments(
-//       documentUnit.value ? documentUnit.value.attachments.length - 1 : 0,
-//     )
-//   }
-// }
-
-// const handleKeyDown = (event: KeyboardEvent) => {
-//   // List of tag names where shortcuts should be disabled
-//   const tagName = (event.target as HTMLElement).tagName.toLowerCase()
-
-//   // Check if the active element is an input, textarea, or any element with contenteditable
-//   if (
-//     ["input", "textarea", "select"].includes(tagName) ||
-//     (event.target as HTMLElement).isContentEditable
-//   ) {
-//     if (event.key === "Escape") (event.target as HTMLElement).blur() // Remove focus from the input field
-//     return // Do nothing if the user is typing in an input field or editable area
-//   }
-
-//   switch (event.key) {
-//     case "<":
-//       event.preventDefault()
-//       toggleNavigationPanel(extraContentSidePanelStore.togglePanel())
-//       break
-//     case "n":
-//       event.preventDefault()
-//       extraContentSidePanelStore.togglePanel(true)
-//       extraContentSidePanelStore.setSidePanelMode("note")
-//       break
-//     case "d":
-//       event.preventDefault()
-//       extraContentSidePanelStore.togglePanel(true)
-//       extraContentSidePanelStore.setSidePanelMode("attachments")
-//       break
-//     case "v":
-//       extraContentSidePanelStore.togglePanel(true)
-//       extraContentSidePanelStore.setSidePanelMode("preview")
-//       break
-//     case "r":
-//       extraContentSidePanelStore.togglePanel(true)
-//       extraContentSidePanelStore.setSidePanelMode("category-import")
-//       break
-//     default:
-//       break
-//   }
-// }
-
-// onBeforeUnmount(() => {
-//   // Remove the event listener when the component is unmounted
-//   window.removeEventListener("keydown", handleKeyDown)
-// })
-
-// onMounted(async () => {
-//   window.addEventListener("keydown", handleKeyDown)
-//   await requestDocumentUnitFromServer()
-// })
 </script>
 
 <template>
   <div class="flex w-screen grow">
-    <!-- v-if="!route.path.includes('preview') && documentUnit" -->
     <div class="sticky top-0 z-50 flex flex-col border-r-1 border-solid border-gray-400 bg-white">
       <SideToggle
         class="sticky top-0 z-20"
@@ -149,43 +31,15 @@ function toggleNavigationPanel(expand?: boolean) {
         <NavbarSide :is-child="false" :menu-items="menuItems" :route="route" />
       </SideToggle>
     </div>
-    <!-- v-if="documentUnit" -->
     <div class="flex w-full min-w-0 flex-col bg-gray-100">
-      <!-- <DocumentUnitInfoPanel
-        v-if="documentUnit && !route.path.includes('preview')"
-        data-testid="document-unit-info-panel"
-        :heading="documentUnit?.documentNumber ?? ''"
-      /> -->
       <DocumentUnitInfoPanel data-testid="document-unit-info-panel" :heading="'KSNR054920707'" />
       <div class="flex grow flex-col items-start">
-        <!-- v-if="documentUnit" -->
         <FlexContainer
           class="h-full w-full flex-grow"
           :class="route.path.includes('preview') ? 'flex-row bg-white' : 'flex-row-reverse'"
-          >Hallo Welt
-          <!-- <ExtraContentSidePanel
-            v-if="
-              documentUnit &&
-              !(
-                route.path.includes('handover') ||
-                route.path.includes('preview')
-              )
-            "
-            :document-unit="documentUnit"
-          ></ExtraContentSidePanel>
-          <router-view
-            :validation-errors="validationErrors"
-            @attachment-index-deleted="attachmentIndexDeleted"
-            @attachment-index-selected="attachmentIndexSelected"
-            @attachments-uploaded="attachmentsUploaded"
-          /> -->
-        </FlexContainer>
+          >Hallo Welt</FlexContainer
+        >
       </div>
     </div>
-    <!-- <ErrorPage
-      v-if="responseError"
-      :error="responseError"
-      :title="responseError?.title"
-    /> -->
   </div>
 </template>
