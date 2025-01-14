c551d76f6fd29b80ce6a19966d56895ef932ad3a
andreas-deazevedo <andreas.deazevedo@digitalservice.bund.de>
2025-01-14 10:58:19 +0100

    RISDEV-6042 Add unit tests

--- frontend/src/components/periodical/DocumentUnitReferenceInput.spec.ts
+++ frontend/src/components/periodical/DocumentUnitReferenceInput.spec.ts
@@ -7,13 +7,15 @@ import { userEvent } from '@testing-library/user-event'
 
 function renderComponent(
   options: {
-    modelValue?: Reference
+    modelValue?: Reference,
+    modelValueList?: Reference[],
   } = {},
 ) {
   return {
     ...render(DocumentUnitReferenceInput, {
       props: {
         modelValue: options.modelValue,
+        modelValueList: options.modelValueList
       },
     }),
   }
@@ -37,11 +39,7 @@ describe('DocumentUnitReferenceInput', () => {
   })
 
   it('item is selected', async () => {
-    renderComponent({
-      modelValue: new Reference(
-        new LegalPeriodical({ title: 'Bundesanzeiger', abbreviation: 'BAnz' }),
-      ),
-    })
+    renderComponent()
 
     const openDropdownContainer = screen.getByLabelText('Dropdown öffnen')
     await user.click(openDropdownContainer)
@@ -59,21 +57,69 @@ describe('DocumentUnitReferenceInput', () => {
 
   it('selection is cleared', async () => {
     renderComponent({
-      modelValue: new Reference(
-        new LegalPeriodical({ title: 'Bundesanzeiger', abbreviation: 'BAnz' }),
+      modelValue: new Reference({
+          legalPeriodical:
+            new LegalPeriodical({title: 'Bundesanzeiger', abbreviation: 'BAnz'})
+        }
       ),
     })
-
-    const openDropdownContainer = screen.getByLabelText('Dropdown öffnen')
-    await user.click(openDropdownContainer)
-    await vi.advanceTimersByTimeAsync(debounceTimeout)
-
-    const dropdownItems = screen.getAllByLabelText('dropdown-option')
-    await user.click(dropdownItems[0])
-    await vi.advanceTimersByTimeAsync(debounceTimeout)
     const resetButton = screen.getByLabelText('Auswahl zurücksetzen')
     await user.click(resetButton)
 
     expect(screen.queryByDisplayValue('BAnz')).not.toBeInTheDocument()
   })
+
+  it('add reference', async () => {
+    const {emitted} = renderComponent()
+
+    const openDropdownContainer = screen.getByLabelText('Dropdown öffnen')
+    await user.click(openDropdownContainer)
+
+    const dropdownItems = screen.getAllByLabelText('dropdown-option')
+    await user.click(dropdownItems[0])
+
+    const citationInput = screen.getByLabelText('Zitatstelle')
+    await user.type(citationInput, 'abcde')
+
+    expect(screen.getByLabelText('Zitatstelle')).toHaveValue('abcde')
+
+    const addButton = screen.getByLabelText('Fundstelle speichern')
+    await user.click(addButton)
+
+    // Expect event 'addEntry' is triggered
+    expect(emitted()['addEntry']).toHaveLength(1)
+    expect(screen.getByDisplayValue('BAnz')).toBeInTheDocument()
+  })
+
+  it('delete reference', async () => {
+    const {emitted, rerender} = renderComponent({
+      modelValue: new Reference({
+        legalPeriodical: new LegalPeriodical({title: 'Bundesanzeiger', abbreviation: 'BAnz'}), citation: 'abcde',
+      }),
+    })
+    const deleteButton = screen.getByLabelText('Eintrag löschen')
+
+    expect(deleteButton).toBeVisible()
+    await user.click(deleteButton)
+    expect(emitted()['removeEntry']).toHaveLength(1)
+
+    // Simulate the removing of the value (triggered by EditableList)
+    await rerender( {modelValue: undefined})
+    expect(screen.queryByLabelText('Eintrag löschen')).not.toBeInTheDocument()
+    expect(screen.queryByDisplayValue('BAnz')).not.toBeInTheDocument()
+  })
+
+  it('cancel editing reference', async () => {
+    renderComponent({
+      modelValue: new Reference({
+        legalPeriodical: new LegalPeriodical({title: 'Bundesanzeiger', abbreviation: 'BAnz'}), citation: 'abcde',
+      }),
+    });
+    const cancelButton = screen.getByLabelText('Abbrechen')
+
+    expect(cancelButton).toBeEnabled()
+    await user.click(cancelButton)
+    expect(screen.getByDisplayValue('BAnz')).toBeInTheDocument()
+  })
+
 })
