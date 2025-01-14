80dc6efffd3b5449770e8ea5a5d139ebdb5f6c4c
andreas-deazevedo <andreas.deazevedo@digitalservice.bund.de>
2025-01-14 14:50:01 +0100

    RISDEV-6042 Rename PeriodicalPage to FundstellenPage and add e2e test

---
+++ frontend/e2e/FundstellenPage.spec.ts
@@ -0,0 +1,48 @@
+import {expect, test} from '@playwright/test'
+
+// See here how to get started:
+// https://playwright.dev/docs/intro
+test.describe('FundstellenPage', () => {
+
+  test(
+    'Visiting the app root url and clicking "Neue Dokumentationseinheit", the view shows the title "Fundstellen", two input fields, a sidebar navigation panel and a save button',
+    {tag: ['@RISDEV-6042']},
+    async ({page}) => {
+      // Arrange
+      await page.goto('/')
+
+      // Action
+      await page.getByRole('button', {name: 'Neue Dokumentationseinheit'}).click()
+
+      // Assert
+      expect(page.url()).toMatch(/.*\/fundstellen$/)
+      await expect(page.getByText('Fundstellen')).toHaveCount(2)
+      await expect(page.getByText('Fundstellen')).toHaveCount(2) // nav bar + title
+      await expect(page.getByText('KSNR054920707')).toHaveCount(1)
+      await expect(page.getByText('Platzhaltertext')).toHaveCount(1)
+      await expect(page.getByText('Unveröffentlicht')).toHaveCount(1)
+      await expect(page.getByText('Fundstellen')).toHaveCount(2)
+      await expect(page.getByTestId('save-button')).toHaveCount(1)
+      await expect(page.getByText('Periodikum')).toHaveCount(1)
+      await expect(page.getByText('Zitatstelle')).toHaveCount(1)
+      await expect(page.getByRole("textbox")).toHaveCount(2)
+    })
+
+  test(
+    'Visiting the "Fundstellen" page and add a fundstelle',
+    {tag: ['@RISDEV-6042']},
+    async ({page}) => {
+      // Arrange
+      await page.goto('/documentUnit/KSNR054920707/fundstellen')
+
+      // Action
+      await page.getByRole('button', {name: 'Dropdown öffnen'}).click()
+      await page.getByText('BAnz | Bundesanzeiger').click()
+      await page.getByLabel('Zitatstelle').fill('2024, Seite 24')
+      await page.getByText('Übernehmen').click()
+
+      // Assert
+      await expect(page.getByText('BAnz 2024, Seite 24')).toHaveCount(1)
+    },
+  )
+})
