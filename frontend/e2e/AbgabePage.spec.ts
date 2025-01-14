7e8c21691b86c4398c5c1948f7cc21c4b96ea270
Sebastian Reck <sebastian.reck@digitalservice.bund.de>
2025-01-14 16:48:58 +0100

    add e2d test to meet acceptance criteria
    RISDEV-6048

---
+++ frontend/e2e/AbgabePage.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test'
+
+test(
+  'Visiting the Abgabe step of creating a documentUnit which displays a Button to end the process and leads to the StartPage',
+  { tag: ['@RISDEV-6048'] },
+  async ({ page }) => {
+    // In the future the new documentUnit needs to be mocked
+    // As this functionality is not there yet we can simply enter the desired page and finish the process
+    await page.goto('/documentUnit/KSNR054920707/abgabe')
+    await expect(page.getByText('Abgabe')).toHaveCount(2)
+    await expect(page.getByText('Zur Veröffentlichung freigeben')).toHaveCount(1)
+    await page.click('text=Zur Veröffentlichung freigeben')
+    await expect(page).toHaveURL('') // Check if the user is redirected to the StartPage
+  },
+)
