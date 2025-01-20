a05a8c6d066d082192b3b4e6c52910c547b46fb0
Hannes <hannes.michalek@digitalservice.bund.de>
2025-01-20 13:35:13 +0100

    Generate HTML report artifacts on e2e tests (#79)

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -29,7 +29,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html'], ['json', { outputFile: 'frontend-e2e-test-report.json' }]],
+  reporter: [
+    ['html', { outputFolder: 'frontend-e2e-test-report-html' }],
+    ['json', { outputFile: 'frontend-e2e-test-report.json' }],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     viewport: { width: 1280, height: 720 },
