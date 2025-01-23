68e0036e6026a9c0acdb3ccb9040fabed474d999
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2025-01-23 10:23:41 +0100

    Tag with Jira ticket

--- frontend/e2e/RubrikenPage.spec.ts
+++ frontend/e2e/RubrikenPage.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test'
 
 test(
   'Visiting the Rubriken step of creating a documentUnit',
-  { tag: ['@RISDEV-6043'] },
+  { tag: ['@RISDEV-6043', '@RISDEV-6047'] },
   async ({ page }) => {
     // In the future the new documentUnit needs to be mocked
     // As this functionality is not there yet we can simply enter the desired page and finish the process
