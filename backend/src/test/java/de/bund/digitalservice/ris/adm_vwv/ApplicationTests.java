c5fa2b023541c3e07fe2a128c0b5e4a1f688b1c5
andreas-deazevedo <andreas.deazevedo@digitalservice.bund.de>
2025-01-24 10:51:08 +0100

    RISDEV 6340 Add setup for minimal backend

---
+++ backend/src/test/java/de/bund/digitalservice/ris/adm_vwv/ApplicationTests.java
@@ -0,0 +1,15 @@
+package de.bund.digitalservice.ris.adm_vwv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ backend/src/test/java/de/bund/digitalservice/ris/adm_vwv/TestApplication.java
@@ -0,0 +1,11 @@
+package de.bund.digitalservice.ris.adm_vwv;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(Application::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
