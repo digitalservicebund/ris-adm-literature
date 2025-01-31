4ae3e57970b38e3c31e876567c11f1a0cc3b863e
Sebastian Reck <sebastian.reck@digitalservice.bund.de>
2025-01-31 16:30:55 +0100

    repair e2e flakyness #2

--- frontend/e2e/FundstellenPage.spec.ts
+++ frontend/e2e/FundstellenPage.spec.ts
@@ -1,18 +1,17 @@
-import {expect, test} from '@playwright/test'
+import { expect, test } from '@playwright/test'
 
 // See here how to get started:
 // https://playwright.dev/docs/intro
 test.describe('FundstellenPage', () => {
-
   test(
     'Visiting the app root url and clicking "Neue Dokumentationseinheit", the view shows the title "Fundstellen", two input fields, a sidebar navigation panel and a save button',
-    {tag: ['@RISDEV-6042']},
-    async ({page}) => {
+    { tag: ['@RISDEV-6042'] },
+    async ({ page }) => {
       // Arrange
       await page.goto('/')
 
       // Action
-      await page.getByRole('button', {name: 'Neue Dokumentationseinheit'}).click()
+      await page.getByRole('button', { name: 'Neue Dokumentationseinheit' }).click()
 
       // Assert
       expect(page.url()).toMatch(/.*\/fundstellen$/)
@@ -25,18 +24,19 @@ test.describe('FundstellenPage', () => {
       await expect(page.getByTestId('save-button')).toHaveCount(1)
       await expect(page.getByText('Periodikum')).toHaveCount(1)
       await expect(page.getByText('Zitatstelle')).toHaveCount(1)
-      await expect(page.getByRole("textbox")).toHaveCount(2)
-    })
+      await expect(page.getByRole('textbox')).toHaveCount(2)
+    },
+  )
 
   test(
     'Visiting the "Fundstellen" page and add a fundstelle',
-    {tag: ['@RISDEV-6042']},
-    async ({page}) => {
+    { tag: ['@RISDEV-6042'] },
+    async ({ page }) => {
       // Arrange
       await page.goto('/documentUnit/KSNR054920707/fundstellen')
 
       // Action
-      await page.getByRole('button', {name: 'Dropdown öffnen'}).click()
+      await page.getByRole('button', { name: 'Dropdown öffnen' }).click()
       await page.getByText('BAnz | Bundesanzeiger').click()
       await page.getByLabel('Zitatstelle').fill('2024, Seite 24')
       await page.getByText('Übernehmen').click()
@@ -48,13 +48,13 @@ test.describe('FundstellenPage', () => {
 
   test(
     'Visiting the "Fundstellen" page, add a fundstelle, edit and close',
-    {tag: ['@RISDEV-6042']},
-    async ({page}) => {
+    { tag: ['@RISDEV-6042'] },
+    async ({ page }) => {
       // Arrange
       await page.goto('/documentUnit/KSNR054920707/fundstellen')
 
       // Action
-      await page.getByRole('button', {name: 'Dropdown öffnen'}).click()
+      await page.getByRole('button', { name: 'Dropdown öffnen' }).click()
       await page.getByText('AA | Arbeitsrecht aktiv').click()
       await page.getByLabel('Zitatstelle').fill('1991, Seite 92')
       await page.getByText('Übernehmen').click()
@@ -68,18 +68,18 @@ test.describe('FundstellenPage', () => {
 
   test(
     'Visiting the "Fundstellen" page, add two item of fundstelle, delete the first item',
-    {tag: ['@RISDEV-6042']},
-    async ({page}) => {
+    { tag: ['@RISDEV-6042'] },
+    async ({ page }) => {
       // Arrange
       await page.goto('/documentUnit/KSNR054920707/fundstellen')
 
       // Action
-      await page.getByRole('button', {name: 'Dropdown öffnen'}).click()
+      await page.getByRole('textbox', { name: 'Periodikum' }).click()
       await page.getByText('AA | Arbeitsrecht aktiv').click()
       await page.getByLabel('Zitatstelle').fill('1991, Seite 92')
       await page.getByText('Übernehmen').click()
 
-      await page.getByRole('button', {name: 'Dropdown öffnen'}).click()
+      await page.getByRole('textbox', { name: 'Periodikum' }).click()
       await page.getByText('BAnz | Bundesanzeiger').click()
       await page.getByLabel('Zitatstelle').fill('2001, Seite 21')
       await page.getByText('Übernehmen').click()
