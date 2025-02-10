78bf1093e981f67bccbe249b81b17e6f09090645
andreas-deazevedo <andreas.deazevedo@digitalservice.bund.de>
2025-02-10 15:45:52 +0100

    RISDEV-6626 Add web mvc test

---
+++ backend/src/test/java/de/bund/digitalservice/ris/adm_vwv/adapter/api/DocumentationUnitControllerTest.java
@@ -0,0 +1,45 @@
+package de.bund.digitalservice.ris.adm_vwv.adapter.api;
+
+import de.bund.digitalservice.ris.adm_vwv.application.CreateDocumentationUnitPort;
+import de.bund.digitalservice.ris.adm_vwv.application.DocumentationUnit;
+import de.bund.digitalservice.ris.adm_vwv.config.SecurityConfiguration;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.UUID;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = DocumentationUnitController.class)
+@Import(SecurityConfiguration.class)
+class DocumentationUnitControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private CreateDocumentationUnitPort createDocumentationUnitPort;
+
+    @Test
+    void create() throws Exception {
+        // given
+        UUID id = UUID.randomUUID();
+        given(createDocumentationUnitPort.create())
+                .willReturn(new DocumentationUnit("KSNR054920707", id));
+
+        // when
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/documentation-units"))
+                // then
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(id.toString()))
+                .andExpect(jsonPath("$.documentNumber").value("KSNR054920707"));
+    }
+
+}
