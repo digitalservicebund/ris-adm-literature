c170b08e96d85b1e1980d987afba22a2f62b2fbb
andreas-deazevedo <andreas.deazevedo@digitalservice.bund.de>
2025-02-10 16:36:22 +0100

    RISDEV-6626 Fix integration test

--- backend/src/test/java/de/bund/digitalservice/ris/adm_vwv/application/DocumentationUnitServiceIntegrationTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/adm_vwv/application/DocumentationUnitServiceIntegrationTest.java
@@ -2,10 +2,13 @@ package de.bund.digitalservice.ris.adm_vwv.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import de.bund.digitalservice.ris.adm_vwv.TestcontainersConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
+@Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class DocumentationUnitServiceIntegrationTest {
 
