4db197183710cfd88362c395268763f7d218838c
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2025-02-10 20:46:59 +0100

    Remove obsolete comment

--- frontend/src/components/periodical/ReferenceSummary.spec.ts
+++ frontend/src/components/periodical/ReferenceSummary.spec.ts
@@ -31,8 +31,6 @@ describe('ReferenceSummary', () => {
       },
     })
 
-    // a plain .not.toBeInTheDocument() should be available at some time
-    // cf. https://github.com/vitest-dev/vitest/pull/6634
     expect(() => screen.queryByText('primär')).not.toBeInTheDocument()
   })
 })
