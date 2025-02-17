e51a27c26a26ee22d2135db48df95c26bd6ff3ab
andreas-deazevedo <andreas.deazevedo@digitalservice.bund.de>
2025-02-17 17:48:25 +0100

    Add application ready listener

---
+++ backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/ApplicationReadyListener.java
@@ -0,0 +1,16 @@
+package de.bund.digitalservice.ris.adm_vwv.application;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class ApplicationReadyListener {
+
+  @EventListener(ApplicationReadyEvent.class)
+  public void logAfterStartup() {
+    log.info("\n\n--> NeuRIS ADM VwV Backend Startup completed <--\n");
+  }
+}
