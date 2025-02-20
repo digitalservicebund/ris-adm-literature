8588e3998cb568dfb6c59fa13db150e03f6bd37e
Sebastian Reck <sebastian.reck@digitalservice.bund.de>
2025-02-20 17:08:18 +0100

    refine trivy script

--- .lefthook/pre-commit/trivy-secrets.sh
+++ .lefthook/pre-commit/trivy-secrets.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-trivy fs --scanners secret -q --skip-dirs frontend/node_modules,frontend/coverage,frontend/html,frontend/test-results,backend/.idea,backend/.gradle,backend/.build .
+errors=$(trivy fs --scanners secret -q --skip-dirs frontend/node_modules,frontend/coverage,frontend/html,frontend/test-results,backend/.idea,backend/.gradle,backend/.build -f json . | jq '.Results | length')
+
+if [ $errors -eq "0" ] ; then
+    exit 0
+fi
+exit 1
