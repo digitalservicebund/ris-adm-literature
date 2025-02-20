805cd1444dd5c70a0747f3202645fb30966d4c72
Sebastian Reck <sebastian.reck@digitalservice.bund.de>
2025-02-20 17:11:50 +0100

    make trivy more verbose

--- .lefthook/pre-commit/trivy-secrets.sh
+++ .lefthook/pre-commit/trivy-secrets.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-errors=$(trivy fs --scanners secret -q --skip-dirs frontend/node_modules,frontend/coverage,frontend/html,frontend/test-results,backend/.idea,backend/.gradle,backend/.build -f json . | jq '.Results | length')
+trivy=$(trivy fs --scanners secret -q --skip-dirs frontend/node_modules,frontend/coverage,frontend/html,frontend/test-results,backend/.idea,backend/.gradle,backend/.build -f json .)
+
+echo $trivy | jq
+
+errors=$(echo $trivy | jq '.Results | length')
 
 if [ $errors -eq "0" ] ; then
     exit 0
