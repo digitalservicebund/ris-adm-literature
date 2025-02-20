857f2fffcaf61a2c4f73952a3b0b7bb8c81c309f
Sebastian Reck <sebastian.reck@digitalservice.bund.de>
2025-02-20 17:12:34 +0100

    but not too verbose... only in error case

--- .lefthook/pre-commit/trivy-secrets.sh
+++ .lefthook/pre-commit/trivy-secrets.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
 trivy=$(trivy fs --scanners secret -q --skip-dirs frontend/node_modules,frontend/coverage,frontend/html,frontend/test-results,backend/.idea,backend/.gradle,backend/.build -f json .)
-
-echo $trivy | jq
-
 errors=$(echo $trivy | jq '.Results | length')
 
 if [ $errors -eq "0" ] ; then
     exit 0
 fi
+echo $trivy | jq
 exit 1
