4026f190f23122aa2106a5be5f36b3240e089e02
Sebastian Reck <sebastian.reck@digitalservice.bund.de>
2025-02-21 12:16:42 +0100

    use native exit code feature of trivy

--- .lefthook/pre-commit/trivy-secrets.sh
+++ .lefthook/pre-commit/trivy-secrets.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 
-trivy=$(trivy fs --scanners secret -q --skip-dirs frontend/node_modules,frontend/coverage,frontend/html,frontend/test-results,backend/.idea,backend/.gradle,backend/.build -f json .)
-errors=$(echo $trivy | jq '.Results | length')
-
-if [ $errors -eq "0" ] ; then
-    exit 0
-fi
-echo $trivy | jq
-exit 1
+trivy fs --scanners secret -q --exit-code 1 --skip-dirs frontend/node_modules,frontend/coverage,frontend/html,frontend/test-results,backend/.idea,backend/.gradle,backend/.build .
