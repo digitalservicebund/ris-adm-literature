fa12b0df5acf2ae60d1052283db9c25af09b852f
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2025-02-22 20:50:56 +0100

    Adjust to changes

--- frontend/src/stores/documentUnitStore.ts
+++ frontend/src/stores/documentUnitStore.ts
@@ -14,7 +14,7 @@ export const useDocumentUnitStore = defineStore('docunitStore', () => {
   ): Promise<ServiceResponse<DocumentUnitResponse>> {
     const response = await documentUnitService.getByDocumentNumber(documentNumber)
     if (response.data) {
-      documentUnit.value = response.data.json
+      documentUnit.value = response.data.documentUnit
     } else {
       documentUnit.value = undefined
     }
@@ -38,7 +38,7 @@ export const useDocumentUnitStore = defineStore('docunitStore', () => {
     const response = await documentUnitService.update(documentUnit.value)
 
     if (response.status === 200) {
-      documentUnit.value = (response.data as DocumentUnitResponse).json
+      documentUnit.value = (response.data as DocumentUnitResponse).documentUnit
     } else {
       return {
         status: response.status,
