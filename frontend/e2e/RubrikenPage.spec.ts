3ec1a26c2d9e107cfd138126f0b493f8a9a9f234
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2025-02-24 21:18:47 +0100

    Move Normverweis tests to separate file

--- frontend/e2e/RubrikenPage.spec.ts
+++ frontend/e2e/RubrikenPage.spec.ts
@@ -49,59 +49,6 @@ test.describe('RubrikenPage', () => {
     },
   )
 
-  test('Add a norm, edit and save', { tag: ['@RISDEV-6075'] }, async ({ page }) => {
-    // given
-    await page.goto('/documentUnit/KSNR054920707/fundstellen')
-    await page.getByText('Rubriken').click()
-    await expect(page.getByText('Rubriken')).toHaveCount(1)
-    await expect(
-      page.getByTestId('normReferences').getByRole('textbox', { name: 'RIS-Abkürzung' }),
-    ).toHaveCount(1)
-
-    // when
-    await page.getByTestId('normReferences').getByRole('textbox', { name: 'RIS-Abkürzung' }).click()
-    await expect(page.getByText('KVLG')).toBeVisible()
-    await page.getByText('KVLG').click()
-    await page.getByRole('textbox', { name: 'Einzelnorm der Norm' }).fill('§ 2')
-    await page.getByRole('button', { name: 'Norm speichern' }).click()
-    await page.getByTestId('list-entry-0').click()
-    await page.getByRole('textbox', { name: 'Fassungsdatum' }).fill('27.01.2025')
-    await page.getByRole('button', { name: 'Norm speichern' }).click()
-
-    // then
-    await expect(page.getByText('KVLG, § 2, 27.01.2025')).toHaveCount(1)
-  })
-
-  test('Add two norms, delete the first item', { tag: ['@RISDEV-6075'] }, async ({ page }) => {
-    // given
-    await page.goto('/documentUnit/KSNR054920707/fundstellen')
-    await page.getByText('Rubriken').click()
-    await expect(page.getByText('Rubriken')).toHaveCount(1)
-    await expect(
-      page.getByTestId('normReferences').getByRole('textbox', { name: 'RIS-Abkürzung' }),
-    ).toHaveCount(1)
-
-    // when
-    await page.getByTestId('normReferences').getByRole('textbox', { name: 'RIS-Abkürzung' }).click()
-    await expect(page.getByText('SGB 5')).toBeVisible()
-    await page.getByText('SGB 5').click()
-    await page.getByRole('textbox', { name: 'Einzelnorm der Norm' }).fill('1991, Seite 92')
-    await page.getByRole('button', { name: 'Norm speichern' }).click()
-    await page.getByTestId('normReferences').getByRole('textbox', { name: 'RIS-Abkürzung' }).click()
-    await expect(page.getByText('KVLG')).toBeVisible()
-    await page.getByText('KVLG').click()
-    await page.getByRole('textbox', { name: 'Einzelnorm der Norm' }).fill('§ 2')
-    await page.getByRole('button', { name: 'Norm speichern' }).click()
-    await page.getByTestId('list-entry-0').click()
-    await page.getByText('Eintrag löschen').click()
-
-    // then
-    await expect(page.getByText('SGB 5, 1991, Seite 92')).toHaveCount(0)
-    await expect(page.getByText('KVLG, § 2')).toHaveCount(1)
-  })
-
-
-
   test('Add an active reference, edit and save', { tag: ['@RISDEV-6074'] }, async ({ page }) => {
     // given
     await page.goto('/documentUnit/KSNR054920707/fundstellen')
