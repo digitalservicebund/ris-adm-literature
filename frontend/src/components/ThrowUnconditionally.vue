947d52a22496ae633e0d9e73374802dd64ad1a56
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2025-02-27 15:34:17 +0100

    Throw error on /throwUnconditionally

---
+++ frontend/src/components/ThrowUnconditionally.vue
@@ -0,0 +1,6 @@
+<script lang="ts" setup>
+console.log('Throwing an error unconditionally')
+throw new Error('Error thrown unconditionally')
+</script>
+
+<template>This page throws an error.</template>
