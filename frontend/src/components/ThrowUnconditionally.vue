668be81d8122cd82011593598f2864d140895415
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2025-02-27 16:46:40 +0100

    Sentry.capture, throw and log both.

--- frontend/src/components/ThrowUnconditionally.vue
+++ frontend/src/components/ThrowUnconditionally.vue
@@ -1,7 +1,13 @@
 <script lang="ts" setup>
 import * as Sentry from '@sentry/vue'
 
+console.log('Capturing a sentry error unconditionally ')
+
 Sentry.captureException('Error thrown unconditionally')
+
+console.log('Throwing an error unconditionally ')
+
+throw new Error('Error thrown unconditionally')
 </script>
 
 <template>This page throws a sentry error.</template>
