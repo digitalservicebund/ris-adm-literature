1594d9a0177126a19d48d6b20543fe394215ba5e
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2025-02-27 21:36:04 +0100

    Remove route that threw errors unconditionally (for setting up sentry)

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -6,7 +6,6 @@ import DocumentUnitWrapper from './routes/documentUnit/[documentNumber].vue'
 import AbgabePage from './routes/documentUnit/[documentNumber]/AbgabePage.vue'
 import RubrikenPage from './routes/documentUnit/[documentNumber]/RubrikenPage.vue'
 import FundstellenPage from '@/routes/documentUnit/[documentNumber]/FundstellenPage.vue'
-import ThrowUnconditionally from './components/ThrowUnconditionally.vue'
 
 const router = createRouter({
   history: createWebHistory(),
@@ -56,12 +55,6 @@ const router = createRouter({
           component: AbgabePage,
         },
       ],
-    },
-    {
-      path: '/throwUnconditionally',
-      name: 'ThrowUncoditionally',
-      props: true,
-      component: ThrowUnconditionally,
     },
     {
       // cf. https://router.vuejs.org/guide/essentials/dynamic-matching.html
