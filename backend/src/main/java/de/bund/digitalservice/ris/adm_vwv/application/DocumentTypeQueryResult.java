8452a09336cc10bb980926c09ccdea9004efa0f9
andreas-deazevedo <andreas.deazevedo@digitalservice.bund.de>
2025-03-19 11:31:53 +0100

    RISDEV-7128 Remove unused classes

--- backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/DocumentTypeQueryResult.java
+++
@@ -1,6 +0,0 @@
-package de.bund.digitalservice.ris.adm_vwv.application;
-
-import javax.annotation.Nonnull;
-import java.util.List;
-
-public record DocumentTypeQueryResult(@Nonnull List<DocumentType> documentTypes, @Nonnull Pagination pagination) {}
--- backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/Pagination.java
+++
@@ -1,3 +0,0 @@
-package de.bund.digitalservice.ris.adm_vwv.application;
-
-public record Pagination(int totalPages, long totalElements, int numberOfElements) {}
