136d4618fb8d83681a77d13690de9a018b1711d6
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2025-03-24 21:06:05 +0100

    Add javadocs on PageQuery

--- backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/PageQuery.java
+++ backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/PageQuery.java
@@ -3,6 +3,15 @@ package de.bund.digitalservice.ris.adm_vwv.application;
 import javax.annotation.Nonnull;
 import org.springframework.data.domain.Sort;
 
+/**
+ * Details on pagination and sorting
+ * 
+ * @param paged Query with pagination?
+ * @param page What page (of pagination) to return
+ * @param size How many elements in a page (of pagination)
+ * @param sortBy What column to sort by
+ * @param sortDirection Sort ascending or descending?
+ */
 public record PageQuery(
   int page,
   int size,
