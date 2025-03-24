3a84258b3b2bd0dcec04dd8518e1bcb97266402d
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2025-03-24 21:12:24 +0100

    Add javadoc for ApplicationReadyListener

--- backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/ApplicationReadyListener.java
+++ backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/ApplicationReadyListener.java
@@ -5,6 +5,9 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+/**
+ * Respond to application startup completion
+ */
 @Component
 @Slf4j
 public class ApplicationReadyListener {
