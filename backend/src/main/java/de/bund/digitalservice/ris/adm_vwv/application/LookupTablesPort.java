aefd4e3651c77f70e85a777c1d53962542a30fef
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2025-03-24 21:03:02 +0100

    Add javadocs for LookupTablesPort

--- backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/LookupTablesPort.java
+++ backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/LookupTablesPort.java
@@ -3,6 +3,9 @@ package de.bund.digitalservice.ris.adm_vwv.application;
 import javax.annotation.Nonnull;
 import org.springframework.data.domain.Page;
 
+/**
+ * The port for looking up document types
+ */
 public interface LookupTablesPort {
   Page<DocumentType> findBySearchQuery(@Nonnull DocumentTypeQuery query);
 }
