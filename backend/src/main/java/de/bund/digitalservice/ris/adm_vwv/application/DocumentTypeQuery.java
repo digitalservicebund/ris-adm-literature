96bf1a392e62053d7b6b843f5e61b79b67ca98dc
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2025-03-24 20:59:57 +0100

    Add javadocs on DocumentTypeQuery

--- backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/DocumentTypeQuery.java
+++ backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/DocumentTypeQuery.java
@@ -2,4 +2,10 @@ package de.bund.digitalservice.ris.adm_vwv.application;
 
 import javax.annotation.Nonnull;
 
+/**
+ * The query used for looking up document types
+ * 
+ * @param searchQuery String to search the document types' names for
+ * @param pageQuery Details on pagination and sorting
+ */
 public record DocumentTypeQuery(String searchQuery, @Nonnull PageQuery pageQuery) {}
