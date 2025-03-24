cc0550bc7446a0c2ac8d929cc123228a71fd6417
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2025-03-24 21:10:55 +0100

    Add javadoc on DocumentType

--- backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/DocumentType.java
+++ backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/DocumentType.java
@@ -2,4 +2,10 @@ package de.bund.digitalservice.ris.adm_vwv.application;
 
 import jakarta.annotation.Nonnull;
 
+/**
+ * DocumentType as fetched from lookup table
+ * 
+ * @param abbreviation The abbreviated name of the documen type
+ * @param name The full / official of the document type
+ */
 public record DocumentType(@Nonnull String abbreviation, @Nonnull String name) {}
