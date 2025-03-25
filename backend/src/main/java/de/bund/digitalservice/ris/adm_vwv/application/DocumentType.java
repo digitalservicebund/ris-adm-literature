4c5a6a2fd9ceb5c67c29baa25438dd6ce460e470
Hannes <hannes.michalek@digitalservice.bund.de>
2025-03-25 10:01:01 +0100

    Update backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/DocumentType.java

--- backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/DocumentType.java
+++ backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/DocumentType.java
@@ -3,7 +3,7 @@ package de.bund.digitalservice.ris.adm_vwv.application;
 import jakarta.annotation.Nonnull;
 
 /**
- * DocumentType as fetched from lookup table
+ * DocumentType business object
  *
  * @param abbreviation The abbreviated name of the documen type
  * @param name The full / official of the document type
