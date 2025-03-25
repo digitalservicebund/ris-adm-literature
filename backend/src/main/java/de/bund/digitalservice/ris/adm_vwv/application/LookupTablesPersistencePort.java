66928aa0bd3abe3ca98a30cd227f4f8f6039bd95
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2025-03-25 08:00:06 +0100

    Add javadoc for LookupTablesPersistencePort

--- backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/LookupTablesPersistencePort.java
+++ backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/LookupTablesPersistencePort.java
@@ -3,6 +3,9 @@ package de.bund.digitalservice.ris.adm_vwv.application;
 import javax.annotation.Nonnull;
 import org.springframework.data.domain.Page;
 
+/**
+ * Port for lookup table access
+ */
 public interface LookupTablesPersistencePort {
   Page<DocumentType> findBySearchQuery(@Nonnull DocumentTypeQuery query);
 }
