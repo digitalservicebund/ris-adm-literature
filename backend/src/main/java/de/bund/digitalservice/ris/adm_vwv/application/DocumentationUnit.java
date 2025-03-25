d48fa5c55b7cedbd025c4d0bbfffee75c671e072
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2025-03-25 08:03:45 +0100

    Add javadoc for DocumentationUnit

--- backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/DocumentationUnit.java
+++ backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/DocumentationUnit.java
@@ -4,7 +4,13 @@ import com.fasterxml.jackson.annotation.JsonRawValue;
 import jakarta.annotation.Nonnull;
 import java.util.UUID;
 
-/** Documentation unit. */
+/**
+ * DocumentUnit
+ *
+ * @param documentNumber The publicly known ID of the document
+ * @param id The internal (database) id of the document
+ * @param json The JSON containing the documentation unit (as required by the frontend's pinia store)
+ */
 public record DocumentationUnit(
   @Nonnull String documentNumber,
   @Nonnull UUID id,
