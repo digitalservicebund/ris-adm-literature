52e962adb35035c19b169ad0d1b3b92a92514874
Sebastian Reck <sebastian.reck@digitalservice.bund.de>
2025-03-25 18:24:40 +0100

    Remove unused content from api response for field of law
    RISDEV-6315

--- backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/adapter/api/FieldOfLawQueryResponse.java
+++ backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/adapter/api/FieldOfLawQueryResponse.java
@@ -1,13 +1,11 @@
 package de.bund.digitalservice.ris.adm_vwv.adapter.api;
 
 import de.bund.digitalservice.ris.adm_vwv.application.FieldOfLaw;
-import java.util.List;
 import org.springframework.data.domain.Page;
 
 /**
  * Field of law query response.
  *
- * @param fieldsOfLaw Fields of law as list
  * @param page Pagination data
  */
-public record FieldOfLawQueryResponse(List<FieldOfLaw> fieldsOfLaw, Page<FieldOfLaw> page) {}
+public record FieldOfLawQueryResponse(Page<FieldOfLaw> page) {}
