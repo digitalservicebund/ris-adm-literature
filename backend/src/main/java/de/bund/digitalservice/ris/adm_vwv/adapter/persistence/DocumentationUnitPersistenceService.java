6374aa43c16acee97b4bde89ab9d9c103bd8d65f
Hannes <hannes.michalek@digitalservice.bund.de>
2025-03-25 10:00:25 +0100

    Update backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/adapter/persistence/DocumentationUnitPersistenceService.java

--- backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/adapter/persistence/DocumentationUnitPersistenceService.java
+++ backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/adapter/persistence/DocumentationUnitPersistenceService.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
- * Service for CRUD operations on documentation units
+ * Persistence service for CRUD operations on documentation units
  */
 @Service
 @Slf4j
