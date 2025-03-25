906175f364badd234c273661ec6e8509ab10b508
Hannes <hannes.michalek@digitalservice.bund.de>
2025-03-25 10:07:00 +0100

    Update backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/DocumentTypeQuery.java

--- backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/DocumentTypeQuery.java
+++ backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/DocumentTypeQuery.java
@@ -3,7 +3,7 @@ package de.bund.digitalservice.ris.adm_vwv.application;
 import javax.annotation.Nonnull;
 
 /**
- * The query used for looking up document types
+ * The query business object used for looking up document types
  *
  * @param searchQuery String to search the document types' names for
  * @param pageQuery Details on pagination and sorting
