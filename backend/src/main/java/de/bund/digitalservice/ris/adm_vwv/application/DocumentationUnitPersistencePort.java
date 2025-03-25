69f0f392c945cdbb8d057377c1ca0ec94b1096fb
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2025-03-25 07:57:20 +0100

    Add javadocs for DocumentationUnitPersistencePort

--- backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/DocumentationUnitPersistencePort.java
+++ backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/DocumentationUnitPersistencePort.java
@@ -3,6 +3,9 @@ package de.bund.digitalservice.ris.adm_vwv.application;
 import jakarta.annotation.Nonnull;
 import java.util.Optional;
 
+/**
+ * Port for CRUD operations on DocumentationUnits
+ */
 public interface DocumentationUnitPersistencePort {
   Optional<DocumentationUnit> findByDocumentNumber(@Nonnull String documentNumber);
 
