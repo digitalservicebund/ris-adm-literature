000a6999c59566cb4637daa4d8b74cf194f967ed
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2025-03-25 08:05:34 +0100

    Add javadoc for DocumentationUnitController

--- backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/adapter/api/DocumentationUnitController.java
+++ backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/adapter/api/DocumentationUnitController.java
@@ -8,6 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * Controller for CRUD on documentation units
+ */
 @RestController
 @RequiredArgsConstructor
 public class DocumentationUnitController {
