dadd96a9399a33f97d6da26d7c6f63df0e9842a2
Hannes <hannes.michalek@digitalservice.bund.de>
2025-03-25 10:01:38 +0100

    Update backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/DocumentType.java

--- backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/DocumentType.java
+++ backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/DocumentType.java
@@ -5,7 +5,7 @@ import jakarta.annotation.Nonnull;
 /**
  * DocumentType business object
  *
- * @param abbreviation The abbreviated name of the documen type
+ * @param abbreviation The abbreviated name of the document type (e.g. "VV")
  * @param name The full / official of the document type
  */
 public record DocumentType(@Nonnull String abbreviation, @Nonnull String name) {}
