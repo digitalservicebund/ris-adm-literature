ca906654e25cfd555d132d97bb5ba158878208e8
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2025-03-27 08:19:53 +0100

    Build list of CVEs with libraries, use it in deciding what to re-pin.

--- .github/scripts/pin-libraries-again-reported-by-trivy.sh
+++ .github/scripts/pin-libraries-again-reported-by-trivy.sh
@@ -3,25 +3,35 @@
 # We start with 
 # 1. The list of CVEs that caused pinning
 # 2. The list of CVEs that trivy reported after unpinning
+# 3. The list of pinned libraries
 #
-# Then we figure out what CVEs no longer need pinnig (i.e. part of 1. but not of 2.)
-# and unpin these.
+# Then we 
+# 1. Compile a file with both, CVE and pinned library information
+# 2. Figure out what CVEs no longer need pinnig (i.e. are not reported by trivy if unpinned)
+# 3. Unpin these.
 
-LIST_OF_CVES_FILE="cve-list.txt"
-LIST_OF_TRIVY_CVES_FILE="trivy-cves.txt"
-LIST_OF_CVES_
+LIST_OF_CVES_FILE="cve-list.txt" # expected to exist
+LIST_OF_PINNED_FILE="pinned-deps.txt" # expected to exist
+LIST_OF_TRIVY_CVES_FILE="trivy-cves.txt" # expected to exist
+LIST_OF_CVES_WITH_LIBRARIES="cves-with-libraries.txt" # will be created during operation
 
-echo "Walk through CVEs we started with and check if still needed"
-touch 
+echo "Compile CVEs with libraries (for lookup)"
+paste $LIST_OF_CVES_FILE $LIST_OF_PINNED_FILE > $LIST_OF_CVES_WITH_LIBRARIES
+
+
+echo "Walk through CVEs with libraries and check if still needed (= reported by trivy)"
 while read -r line; do
     CVE=`echo $line | cut -d' ' -f2`
-    
+    LIBRARY_DOMAIN=`echo $line | cut -d'"' -f2`
+    LIBRARY_NAME=`echo $line | cut -d'"' -f4`
+    echo $CVE - $LIBRARY_DOMAIN - $LIBRARY_NAME
+
     if grep -q "$CVE" $LIST_OF_TRIVY_CVES_FILE; then
-        echo "$CVE still found by trivy"
+        echo "--> still found by trivy"
         # remove the comments
 
     else
-        echo "$CVE no longer found by trivy. No more pinning needed."
+        echo "--> no longer found by trivy. No more pinning needed."
     fi
 
-done < $LIST_OF_CVES_FILE
+done < $LIST_OF_CVES_WITH_LIBRARIES
