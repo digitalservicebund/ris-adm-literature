52f1c7ee569585b0037c3cf64902a04e176142be
Sebastian Reck <sebastian.reck@digitalservice.bund.de>
2025-04-11 15:37:48 +0200

    Add view
    RISDEV-6312

---
+++ backend/src/main/resources/db/migration/R__create_view_on_legal_periodical.sql
@@ -0,0 +1,7 @@
+-- View uses foreign schema "lookup_tables", therefore it is repeatable.
+-- This migration is executed every time this script is changed.
+
+CREATE OR REPLACE VIEW legal_periodical_view AS
+SELECT id, abbreviation, title, subtitle, citation_style, juris_id
+FROM lookup_tables.legal_periodical;
+
