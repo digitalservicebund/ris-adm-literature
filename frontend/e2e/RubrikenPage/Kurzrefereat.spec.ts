989662fa208d9c30f2eb4c9149751134ca8f3bb4
Hannes <hannes.michalek@digitalservice.bund.de>
2025-04-30 11:47:40 +0200

    e2e undo and redo buttons in Kurzreferat (RISDEV-7668) (#501)
    * Simplify 'when' section
    * Button "Rückgängig machen" undoes the text change
    * Button "Wiederherstellen" retrieves initial text after undo.

--- frontend/e2e/RubrikenPage/Kurzrefereat.spec.ts
+++ frontend/e2e/RubrikenPage/Kurzrefereat.spec.ts
@@ -12,10 +12,10 @@ test.describe('RubrikenPage - Kurzreferat', () => {
 
       const kurzReferatTitleElement = page.getByText('Kurzreferat')
       await expect(kurzReferatTitleElement).toHaveCount(3)
-
-      // when
       const kurzreferatEditorElement = page.getByTestId('Kurzreferat Editor')
       await expect(kurzreferatEditorElement).toHaveCount(1)
+
+      // when
       await kurzreferatEditorElement.click()
       await page.keyboard.insertText('Kurzreferat Eintrag 123')
       // then
@@ -26,6 +26,37 @@ test.describe('RubrikenPage - Kurzreferat', () => {
       await page.reload()
       // then
       await expect(page.getByText('Kurzreferat Eintrag 123')).toHaveCount(1)
+    },
+  )
+
+  test(
+    'Text changes can be undone and redone using the UI elements',
+    { tag: ['@RISDEV-7668'] },
+    async ({ page }) => {
+      // given
+      await page.goto('/')
+      await page.getByText('Neue Dokumentationseinheit').click()
+      await page.getByText('Rubriken').click()
+
+      const kurzreferatEditorElement = page.getByTestId('Kurzreferat Editor')
+      await kurzreferatEditorElement.click()
+      await page.keyboard.insertText('Kurzreferat: Neuer Text')
+
+      // when
+      const undoButton = page
+        .getByLabel('Kurzreferat Button Leiste')
+        .getByRole('button', { name: 'Rückgängig machen' })
+      undoButton.click()
+      // then
+      await expect(page.getByText('Kurzreferat: Neuer Text')).toHaveCount(0)
+
+      // when
+      const redoButton = page
+        .getByLabel('Kurzreferat Button Leiste')
+        .getByRole('button', { name: 'Wiederherstellen' })
+      redoButton.click()
+      // then
+      await expect(page.getByText('Kurzreferat: Neuer Text')).toHaveCount(1)
     },
   )
 })
