4c628f5e38eaf6006456b9a42f031f681c98e70d
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2025-04-30 08:53:35 +0200

    Red test when button not clicked

--- frontend/e2e/RubrikenPage/Kurzrefereat.spec.ts
+++ frontend/e2e/RubrikenPage/Kurzrefereat.spec.ts
@@ -27,5 +27,29 @@ test.describe('RubrikenPage - Kurzreferat', () => {
       // then
       await expect(page.getByText('Kurzreferat Eintrag 123')).toHaveCount(1)
     },
-  )
+  ),
+    test(
+      'Text changes can be undone and redone using the UI elements',
+      { tag: ['@RISDEV-7668'] },
+      async ({ page }) => {
+        // given
+        await page.goto('/')
+        await page.getByText('Neue Dokumentationseinheit').click()
+        await page.getByText('Rubriken').click()
+
+        const kurzreferatEditorElement = page.getByTestId('Kurzreferat Editor')
+        await expect(kurzreferatEditorElement).toHaveCount(1)
+        await kurzreferatEditorElement.click()
+        await page.keyboard.insertText('Kurzreferat: Neuer Text')
+
+        // when
+        // const undoButton = page
+        //   .getByLabel('Kurzreferat Button Leiste')
+        //   .getByRole('button', { name: 'Rückgängig machen' })
+        // undoButton.click()
+
+        // then
+        await expect(page.getByText('Kurzreferat: Neuer Text')).toHaveCount(0)
+      },
+    )
 })
