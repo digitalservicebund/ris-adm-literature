da8460c36087356f34fca2839ecd37fc927a44d7
Xavier Tremel <xavier.tremel@digitalservice.bund.de>
2025-05-02 18:09:55 +0200

    More tests

--- frontend/src/components/normgeber/NormgeberListItem.spec.ts
+++ frontend/src/components/normgeber/NormgeberListItem.spec.ts
@@ -34,4 +34,40 @@ describe('NormgeberListItem', () => {
     expect(screen.getByLabelText('Normgeber *')).toBeInTheDocument()
     expect(screen.queryByRole('button', { name: 'Normgeber hinzufügen' })).not.toBeInTheDocument()
   })
+
+  it('emits an update when clicking on save', async () => {
+    const { user, emitted } = renderComponent()
+
+    // when
+    await user.click(screen.getByRole('button', { name: 'Normgeber Editieren' }))
+    await user.click(screen.getByRole('button', { name: 'Normgeber übernehmen' }))
+
+    // then
+    expect(emitted('updateNormgeber')).toBeTruthy()
+    expect(screen.getByRole('button', { name: 'Normgeber Editieren' })).toBeInTheDocument()
+  })
+
+  it('leaves edit mode on cancel', async () => {
+    const { user } = renderComponent()
+
+    // when
+    await user.click(screen.getByRole('button', { name: 'Normgeber Editieren' }))
+    await user.click(screen.getByRole('button', { name: 'Abbrechen' }))
+
+    // then
+    expect(screen.getByRole('button', { name: 'Normgeber Editieren' })).toBeInTheDocument()
+    expect(screen.getByText('DEU, new institution')).toBeInTheDocument()
+  })
+
+  it('emits an delete event when clicking on delete', async () => {
+    const { user, emitted } = renderComponent()
+
+    // when
+    await user.click(screen.getByRole('button', { name: 'Normgeber Editieren' }))
+    await user.click(screen.getByRole('button', { name: 'Eintrag löschen' }))
+
+    // then
+    expect(emitted('deleteNormgeber')).toBeTruthy()
+    expect(screen.getByRole('button', { name: 'Normgeber Editieren' })).toBeInTheDocument()
+  })
 })
