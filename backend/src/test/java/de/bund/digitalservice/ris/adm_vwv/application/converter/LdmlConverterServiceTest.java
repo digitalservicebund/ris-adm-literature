954948f46628aa132b23aeaba032886b138add15
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2025-05-06 15:06:08 +0200

    Return ausserkrafttretedatum (red test)

--- backend/src/test/java/de/bund/digitalservice/ris/adm_vwv/application/converter/LdmlConverterServiceTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/adm_vwv/application/converter/LdmlConverterServiceTest.java
@@ -161,4 +161,43 @@ class LdmlConverterServiceTest {
       .extracting(DocumentationUnitContent::inkrafttretedatum)
       .isEqualTo("2025-01-01");
   }
+
+  @Test
+  void convertToBusinessModel_expirationDate() {
+    // given
+    String xml =
+      """
+      <?xml version="1.0" encoding="UTF-8"?>
+      <akn:akomaNtoso
+        xmlns:akn="http://docs.oasis-open.org/legaldocml/ns/akn/3.0"
+        xmlns:ris="http://ldml.neuris.de/metadata/">
+        <akn:doc name="offene-struktur">
+          <akn:meta>
+            <akn:proprietary source="attributsemantik-noch-undefiniert">
+              <ris:metadata>
+                <ris:expirationDate>2025-02-02</ris:expirationDate>
+              </ris:metadata>
+            </akn:proprietary>
+          </akn:meta>
+        </akn:doc>
+      </akn:akomaNtoso>
+      """;
+    DocumentationUnit documentationUnit = new DocumentationUnit(
+      "KSNR20250000001",
+      UUID.randomUUID(),
+      null,
+      xml
+    );
+
+    // when
+    DocumentationUnitContent documentationUnitContent = ldmlConverterService.convertToBusinessModel(
+      documentationUnit
+    );
+
+    // then
+    assertThat(documentationUnitContent)
+      .isNotNull()
+      .extracting(DocumentationUnitContent::ausserkrafttretedatum)
+      .isEqualTo("2025-02-02");
+  }
 }
