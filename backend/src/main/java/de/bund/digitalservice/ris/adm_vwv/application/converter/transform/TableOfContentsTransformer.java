e881bf97fcf4b8914e330ad83aeaedc3221b18eb
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2025-05-07 14:04:54 +0200

    Return empty list if there is no tableOfContents

--- backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/converter/transform/TableOfContentsTransformer.java
+++ backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/converter/transform/TableOfContentsTransformer.java
@@ -1,7 +1,11 @@
 package de.bund.digitalservice.ris.adm_vwv.application.converter.transform;
 
 import de.bund.digitalservice.ris.adm_vwv.application.converter.ldml.AkomaNtoso;
+import de.bund.digitalservice.ris.adm_vwv.application.converter.ldml.Proprietary;
+
 import java.util.List;
+import java.util.Optional;
+
 import lombok.RequiredArgsConstructor;
 
 /**
@@ -18,6 +22,11 @@ public class TableOfContentsTransformer {
    * @return List of table of contents entries or empty list
    */
   public List<String> transform() {
-    return akomaNtoso.getDoc().getMeta().getProprietary().getMetadata().getTableOfContentsEntries();
+    Optional<Proprietary> proprietary = Optional.ofNullable(
+      akomaNtoso.getDoc().getMeta().getProprietary()
+    );
+
+    if (proprietary.isEmpty()) return List.of();
+    else return proprietary.get().getMetadata().getTableOfContentsEntries();
   }
 }
--- backend/src/test/java/de/bund/digitalservice/ris/adm_vwv/application/converter/LdmlConverterServiceTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/adm_vwv/application/converter/LdmlConverterServiceTest.java
@@ -7,6 +7,8 @@ import de.bund.digitalservice.ris.adm_vwv.application.DocumentationUnit;
 import de.bund.digitalservice.ris.adm_vwv.application.converter.business.DocumentationUnitContent;
 import de.bund.digitalservice.ris.adm_vwv.application.converter.business.Reference;
 import de.bund.digitalservice.ris.adm_vwv.test.TestFile;
+
+import java.util.List;
 import java.util.UUID;
 import java.util.stream.Stream;
 import org.assertj.core.api.InstanceOfAssertFactories;
@@ -172,5 +174,39 @@ class LdmlConverterServiceTest {
       .isNotNull()
       .extracting(DocumentationUnitContent::gliederung)
       .isEqualTo(Stream.of("TOC entry 1", "TOC entry 2").toList());
+  }
+
+  @Test
+  void convertToBusinessModel_gliederung_nonexistent() {
+    // given
+        String xml =
+      """
+      <?xml version="1.0" encoding="UTF-8"?>
+      <akn:akomaNtoso
+        xmlns:akn="http://docs.oasis-open.org/legaldocml/ns/akn/3.0"
+        xmlns:ris="http://ldml.neuris.de/metadata/">
+        <akn:doc name="offene-struktur">
+          <akn:meta>
+          </akn:meta>
+        </akn:doc>
+      </akn:akomaNtoso>
+      """;
+    DocumentationUnit documentationUnit = new DocumentationUnit(
+      "KSNR20250000001",
+      UUID.randomUUID(),
+      null,
+      xml
+    );
+
+    // when
+    DocumentationUnitContent documentationUnitContent = ldmlConverterService.convertToBusinessModel(
+      documentationUnit
+    );
+
+    // then
+    assertThat(documentationUnitContent)
+      .isNotNull()
+      .extracting(DocumentationUnitContent::gliederung)
+      .isEqualTo(List.of());
   }
 }
