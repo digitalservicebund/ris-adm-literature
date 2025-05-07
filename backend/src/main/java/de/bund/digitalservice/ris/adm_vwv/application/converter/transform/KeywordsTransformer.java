7fd12e8caa877a7d780afa2831edd9451ccffcbd
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2025-05-07 11:53:31 +0200

    Read keywords from LDML

--- backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/converter/transform/KeywordsTransformer.java
+++ backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/converter/transform/KeywordsTransformer.java
@@ -1,11 +1,8 @@
 package de.bund.digitalservice.ris.adm_vwv.application.converter.transform;
 
 import de.bund.digitalservice.ris.adm_vwv.application.converter.ldml.AkomaNtoso;
-import de.bund.digitalservice.ris.adm_vwv.application.converter.ldml.Proprietary;
-import de.bund.digitalservice.ris.adm_vwv.application.converter.ldml.RisMetadata;
-
+import de.bund.digitalservice.ris.adm_vwv.application.converter.ldml.Keyword;
 import java.util.List;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 
 /**
@@ -17,16 +14,12 @@ public class KeywordsTransformer {
   private final AkomaNtoso akomaNtoso;
 
   /**
-   * Transforms the {@code ExpiryDate} object to a string.
+   * Transforms the {@code Keyword} object to a string.
    *
-   * @return The expiryDate or null if Proprietary or Metadata does not exist
+   * @return The list of keyword values or an empty list
    */
   public List<String> transform() {
-    // return Optional.ofNullable(akomaNtoso.getDoc().getMeta().getProprietary())
-    //   .map(Proprietary::getMetadata)
-    //   .map(RisMetadata::getExpiryDate)
-    //   .orElse(null);
-
-    return List.of();
+    List<Keyword> keywords = akomaNtoso.getDoc().getMeta().getClassification().getKeyword();
+    return keywords.stream().map(Keyword::getValue).toList();
   }
 }
