029fb37df8b3fb8d7b62093e219eee8d84eb4e37
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2025-05-07 14:11:00 +0200

    Simplify

--- backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/converter/transform/TableOfContentsTransformer.java
+++ backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/converter/transform/TableOfContentsTransformer.java
@@ -1,7 +1,6 @@
 package de.bund.digitalservice.ris.adm_vwv.application.converter.transform;
 
 import de.bund.digitalservice.ris.adm_vwv.application.converter.ldml.AkomaNtoso;
-import de.bund.digitalservice.ris.adm_vwv.application.converter.ldml.Proprietary;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
@@ -20,11 +19,8 @@ public class TableOfContentsTransformer {
    * @return List of table of contents entries or empty list
    */
   public List<String> transform() {
-    Optional<Proprietary> proprietary = Optional.ofNullable(
-      akomaNtoso.getDoc().getMeta().getProprietary()
-    );
-
-    if (proprietary.isEmpty()) return List.of();
-    else return proprietary.get().getMetadata().getTableOfContentsEntries();
+    return Optional.ofNullable(akomaNtoso.getDoc().getMeta().getProprietary())
+      .map(proprietary -> proprietary.getMetadata().getTableOfContentsEntries())
+      .orElse(List.of());
   }
 }
