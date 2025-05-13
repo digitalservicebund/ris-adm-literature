893128e7f4843959b39c482a6d04681cd9eae816
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2025-05-13 15:06:39 +0200

    Gliederung: add e2e for undo/redo

--- frontend/e2e/RubrikenPage/Gliederung.spec.ts
+++ frontend/e2e/RubrikenPage/Gliederung.spec.ts
@@ -26,4 +26,35 @@ test.describe('RubrikenPage - Gliederung', () => {
       await expect(page.getByText('Test 123')).toHaveCount(1)
     },
   )
+
+  test(
+    'Text changes can be undone and redone using the UI elements',
+    { tag: ['@RISDEV-7843'] },
+    async ({ page }) => {
+      // given
+      await page.goto('/')
+      await page.getByText('Neue Dokumentationseinheit').click()
+      await page.getByText('Rubriken').click()
+
+      const kurzreferatEditorElement = page.getByTestId('Gliederung Editor')
+      await kurzreferatEditorElement.click()
+      await page.keyboard.insertText('Gliederung: Neuer Text')
+
+      // when
+      const undoButton = page
+        .getByLabel('Gliederung Button Leiste')
+        .getByRole('button', { name: 'Rückgängig machen' })
+      undoButton.click()
+      // then
+      await expect(page.getByText('Gliederung: Neuer Text')).toHaveCount(0)
+
+      // when
+      const redoButton = page
+        .getByLabel('Gliederung Button Leiste')
+        .getByRole('button', { name: 'Wiederherstellen' })
+      redoButton.click()
+      // then
+      await expect(page.getByText('Gliederung: Neuer Text')).toHaveCount(1)
+    },
+  )
 })
