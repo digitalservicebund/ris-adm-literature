10d4152a2744e74bb2b92f93375760adeace5c81
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2025-05-15 21:37:23 +0200

    GET on /api/documentation-units to return HTTP 200 (red test) [no ci]

--- backend/src/test/java/de/bund/digitalservice/ris/adm_vwv/adapter/api/DocumentationUnitControllerIntegrationTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/adm_vwv/adapter/api/DocumentationUnitControllerIntegrationTest.java
@@ -1,21 +1,15 @@
 package de.bund.digitalservice.ris.adm_vwv.adapter.api;
 
-import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import de.bund.digitalservice.ris.adm_vwv.application.DocumentationUnit;
 import de.bund.digitalservice.ris.adm_vwv.application.DocumentationUnitPort;
 import de.bund.digitalservice.ris.adm_vwv.config.SecurityConfiguration;
-import java.util.Optional;
-import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -30,99 +24,14 @@ class DocumentationUnitControllerIntegrationTest {
   private DocumentationUnitPort documentationUnitPort;
 
   @Test
-  @DisplayName("Request GET returns HTTP 200 and data from mocked documentation unit port")
+  @DisplayName("Request GET /api/documentation-units returns HTTP 200")
   void find() throws Exception {
     // given
-    String documentNumber = "KSNR054920707";
-    String json = "{\"test\":\"content\"}";
-    given(documentationUnitPort.findByDocumentNumber(documentNumber)).willReturn(
-      Optional.of(new DocumentationUnit(documentNumber, UUID.randomUUID(), json))
-    );
 
     // when
     mockMvc
-      .perform(get("/api/documentation-units/{documentNumber}", documentNumber))
+      .perform(get("/api/documentation-units"))
       // then
-      .andExpect(status().isOk())
-      .andExpect(jsonPath("$.documentNumber").value(documentNumber))
-      .andExpect(jsonPath("$.json.test").value("content"));
-  }
-
-  @Test
-  @DisplayName(
-    "Request GET returns HTTP 404 because mocked documentation unit port returns empty optional"
-  )
-  void find_notFound() throws Exception {
-    // given
-    String documentNumber = "KSNR000000001";
-    given(documentationUnitPort.findByDocumentNumber(documentNumber)).willReturn(Optional.empty());
-
-    // when
-    mockMvc
-      .perform(get("/api/documentation-units/{documentNumber}", documentNumber))
-      // then
-      .andExpect(status().isNotFound());
-  }
-
-  @Test
-  @DisplayName("Request POST returns HTTP 201 and data from mocked documentation unit port")
-  void create() throws Exception {
-    // given
-    UUID id = UUID.randomUUID();
-    given(documentationUnitPort.create()).willReturn(
-      new DocumentationUnit("KSNR054920707", id, null)
-    );
-
-    // when
-    mockMvc
-      .perform(post("/api/documentation-units"))
-      // then
-      .andExpect(status().isCreated())
-      .andExpect(jsonPath("$.id").value(id.toString()))
-      .andExpect(jsonPath("$.documentNumber").value("KSNR054920707"));
-  }
-
-  @Test
-  @DisplayName("Request PUT returns HTTP 200 and data from mocked documentation unit port")
-  void update() throws Exception {
-    // given
-    String documentNumber = "KSNR054920707";
-    String json = "{\"test\":\"content\"}";
-    given(documentationUnitPort.update(documentNumber, json)).willReturn(
-      Optional.of(new DocumentationUnit(documentNumber, UUID.randomUUID(), json))
-    );
-
-    // when
-    mockMvc
-      .perform(
-        put("/api/documentation-units/{documentNumber}", documentNumber)
-          .content(json)
-          .contentType(MediaType.APPLICATION_JSON)
-      )
-      // then
-      .andExpect(status().isOk())
-      .andExpect(jsonPath("$.documentNumber").value(documentNumber))
-      .andExpect(jsonPath("$.json.test").value("content"));
-  }
-
-  @Test
-  @DisplayName(
-    "Request PUT returns HTTP 404 because mocked documentation unit port returns empty optional"
-  )
-  void update_notFound() throws Exception {
-    // given
-    String documentNumber = "KSNR000000001";
-    String json = "{\"test\":\"unsuccessful\"}";
-    given(documentationUnitPort.update(documentNumber, json)).willReturn(Optional.empty());
-
-    // when
-    mockMvc
-      .perform(
-        put("/api/documentation-units/{documentNumber}", documentNumber)
-          .content(json)
-          .contentType(MediaType.APPLICATION_JSON)
-      )
-      // then
-      .andExpect(status().isNotFound());
+      .andExpect(status().isOk());
   }
 }
