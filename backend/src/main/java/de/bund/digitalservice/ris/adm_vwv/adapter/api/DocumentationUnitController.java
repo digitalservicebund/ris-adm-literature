feba4fc064fe4829baffc399618275ac6d32491d
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2025-05-16 13:46:04 +0200

    Return fundstellen ids

--- backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/adapter/api/DocumentationUnitController.java
+++ backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/adapter/api/DocumentationUnitController.java
@@ -36,7 +36,7 @@ public class DocumentationUnitController {
         "sample dokumentnummer 1",
         "2011-11-11",
         "Sample Document Title 1",
-        List.of(new Fundstelle())
+        List.of(new Fundstelle("fundstellen id 1"), new Fundstelle("fundstellen id 2"))
       ),
       new DocumentationUnitListElement(
         UUID.fromString("22222222-1657-4085-ae2a-993a04c27f6b"),
--- backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/Fundstelle.java
+++ backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/Fundstelle.java
@@ -1,3 +1,7 @@
 package de.bund.digitalservice.ris.adm_vwv.application;
 
-public record Fundstelle() {}
+public record Fundstelle(
+  String id
+  // zitatstelle:string
+  // periodika:Periodikum[]
+) {}
