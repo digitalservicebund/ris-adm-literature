96b37d88bceff44d8201f3d321d49e5b699b5b1c
Xavier Tremel <xavier.tremel@digitalservice.bund.de>
2025-05-26 10:01:20 +0200

    Add RisPaginator

--- frontend/src/components/document-units/DocumentUnits.vue
+++ frontend/src/components/document-units/DocumentUnits.vue
@@ -3,10 +3,11 @@ import type { DocumentUnitListItem } from '@/domain/documentUnit'
 import { onMounted, ref } from 'vue'
 import DocumentUnitList from './DocumentUnitList.vue'
 import documentUnitService from '@/services/documentUnitService'
+import { RisPaginator } from '@digitalservicebund/ris-ui/components'
 
 const ITEMS_PER_PAGE = 100
 
-const isLoading = ref(false)
+const isLoading = ref(true)
 const firstRowIndex = ref<number>(0)
 const totalRows = ref<number>(0)
 const docUnits = ref<DocumentUnitListItem[]>([])
@@ -14,6 +15,9 @@ const docUnits = ref<DocumentUnitListItem[]>([])
 onMounted(async () => {
   const response = await documentUnitService.getPaginatedDocumentUnitList()
   docUnits.value = response.data?.documentationUnitsOverview || []
+  totalRows.value = response.data?.page.numberOfElements || 0
+  firstRowIndex.value = response.data?.page.number || 0
+  isLoading.value = false
 })
 </script>
 
@@ -24,5 +28,10 @@ onMounted(async () => {
     :rows-per-page="ITEMS_PER_PAGE"
     :total-rows="totalRows"
     :loading="isLoading"
+  />
+  <RisPaginator
+    :first="firstRowIndex"
+    :rows="ITEMS_PER_PAGE"
+    :total-records="totalRows"
   />
 </template>
