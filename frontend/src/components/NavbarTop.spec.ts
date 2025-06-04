072606be940886dfe13a15f4a4a6ea121b11fdce
lucas <l.solcher@gmail.com>
2025-06-04 10:15:12 +0200

    Add navbar link test

--- frontend/src/components/NavbarTop.spec.ts
+++ frontend/src/components/NavbarTop.spec.ts
@@ -1,11 +1,20 @@
 import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
-import NavbarTop from './NavbarTop.vue'
+import NavbarTop from '@/components/NavbarTop.vue'
+import { render, screen } from '@testing-library/vue'
 
-describe('HelloWorld', () => {
+describe('NavbarTop', () => {
   it('renders properly', () => {
     const wrapper = mount(NavbarTop, { props: {} })
     expect(wrapper.text()).toContain('Rechtsinformationen')
+  })
+
+  it('renders the main navigation link to the homepage with correct text and href', async () => {
+    render(NavbarTop)
+    const homepageLink = screen.getByRole('link', { name: /Rechtsinformationen des Bundes/i })
+
+    expect(homepageLink).toBeVisible()
+    expect(homepageLink).toHaveAttribute('href', '/')
   })
 })
