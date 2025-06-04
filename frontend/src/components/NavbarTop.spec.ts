592007dc4f18a025c6592a0099a4531278ae8061
lucas <l.solcher@gmail.com>
2025-06-04 11:07:13 +0200

    Revert "Add navbar link test"
    This reverts commit 072606be940886dfe13a15f4a4a6ea121b11fdce.

--- frontend/src/components/NavbarTop.spec.ts
+++ frontend/src/components/NavbarTop.spec.ts
@@ -1,20 +1,11 @@
 import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
-import NavbarTop from '@/components/NavbarTop.vue'
-import { render, screen } from '@testing-library/vue'
+import NavbarTop from './NavbarTop.vue'
 
-describe('NavbarTop', () => {
+describe('HelloWorld', () => {
   it('renders properly', () => {
     const wrapper = mount(NavbarTop, { props: {} })
     expect(wrapper.text()).toContain('Rechtsinformationen')
-  })
-
-  it('renders the main navigation link to the homepage with correct text and href', async () => {
-    render(NavbarTop)
-    const homepageLink = screen.getByRole('link', { name: /Rechtsinformationen des Bundes/i })
-
-    expect(homepageLink).toBeVisible()
-    expect(homepageLink).toHaveAttribute('href', '/')
   })
 })
