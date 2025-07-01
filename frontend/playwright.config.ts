c38a451cf54032e4261c23ad485e9e103bd45828
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2025-07-01 13:25:11 +0200

    Remove unnecessary comments

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -2,12 +2,6 @@ import process from 'node:process'
 import { defineConfig, devices } from '@playwright/test'
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
@@ -42,7 +36,6 @@ export default defineConfig({
     actionTimeout: 5 * 1000,
     /* Base URL to use in actions like `await page.goto('/')`.
     needs to be in sync with "webserver" config below*/
-    // baseURL: process.env.CI ? 'http://localhost:4173' : 'http://localhost:5173',
     baseURL: 'http://localhost:5173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
@@ -77,38 +70,7 @@ export default defineConfig({
         ...devices['Desktop Edge'],
       },
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: {
-    //     ...devices['Pixel 5'],
-    //   },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: {
-    //     ...devices['iPhone 12'],
-    //   },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: {
-    //     channel: 'msedge',
-    //   },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: {
-    //     channel: 'chrome',
-    //   },
-    // },
   ],
-
-  /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  // outputDir: 'test-results/',
 
   /* Run your local dev server before starting the tests */
   webServer: {
