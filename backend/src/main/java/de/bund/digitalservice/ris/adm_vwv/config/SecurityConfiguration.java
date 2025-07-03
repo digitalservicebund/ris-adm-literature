a0253ec22e1dbbdb20d71630cf524655f6e6ad46
lucas <l.solcher@gmail.com>
2025-07-03 09:38:04 +0200

    Update security config

--- backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/config/SecurityConfiguration.java
+++ backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/config/SecurityConfiguration.java
@@ -1,15 +1,26 @@
 package de.bund.digitalservice.ris.adm_vwv.config;
 
-import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
-
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.servlet.util.matcher.PathPatternRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -28,22 +39,70 @@ public class SecurityConfiguration {
     // Define the allowed origins (your frontend's URL)
     configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:8080"));
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-    configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+    configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Cache-Control"));
     configuration.setAllowCredentials(true);
 
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/api/**", configuration);
+    source.registerCorsConfiguration("/**", configuration);
     return source;
   }
 
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    return http
+    http
+      .authorizeHttpRequests(authorize ->
+        authorize
+          // --- PUBLIC ENDPOINTS ---
+          .requestMatchers(
+            "/.well-known/security.txt",
+            "/actuator/health/**",
+            "/actuator/prometheus"
+          )
+          .permitAll()
+          // --- SECURED ENDPOINTS ---
+          .requestMatchers("/api/**") // Secure all API paths
+          .hasRole("VWN_ROLE") // Require the single role
+          // --- DENY ALL OTHERS ---
+          .anyRequest()
+          .denyAll()
+      )
+      .exceptionHandling(configurer ->
+        configurer.defaultAuthenticationEntryPointFor(
+          new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED),
+          PathPatternRequestMatcher.withDefaults().matcher("/api/**")
+        )
+      )
+      .oauth2ResourceServer(oauth2 ->
+        oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverterForKeycloak()))
+      )
       .csrf(AbstractHttpConfigurer::disable)
-      .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-      .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
-      .oauth2ResourceServer(oauth2 -> oauth2.jwt())
-      .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
-      .build();
+      .cors(Customizer.withDefaults())
+      .sessionManagement(sessionManagement ->
+        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+      );
+    return http.build();
+  }
+
+  @Bean
+  @SuppressWarnings("unchecked")
+  public JwtAuthenticationConverter jwtAuthenticationConverterForKeycloak() {
+    final Converter<Jwt, Collection<GrantedAuthority>> jwtGrantedAuthoritiesConverter = jwt -> {
+      final Map<String, Object> realmAccess = jwt.getClaimAsMap("realm_access");
+      if (realmAccess == null || realmAccess.isEmpty()) {
+        return List.of();
+      }
+      final List<String> roles = (List<String>) realmAccess.get("roles");
+      if (roles == null || roles.isEmpty()) {
+        return List.of();
+      }
+      return roles
+        .stream()
+        .map(role -> "ROLE_" + role) // add prefix needed for .hasRole()
+        .map(SimpleGrantedAuthority::new)
+        .collect(Collectors.toList());
+    };
+    var jwtAuthenticationConverter = new JwtAuthenticationConverter();
+    jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+    return jwtAuthenticationConverter;
   }
 }
