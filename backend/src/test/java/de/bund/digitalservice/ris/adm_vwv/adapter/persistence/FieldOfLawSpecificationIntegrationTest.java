b1b7d7fe39eae16523c3a2f57adcac0d710e8cf0
lucas <l.solcher@gmail.com>
2025-07-16 10:04:20 +0200

    Cleanup

--- backend/src/test/java/de/bund/digitalservice/ris/adm_vwv/adapter/persistence/FieldOfLawSpecificationIntegrationTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/adm_vwv/adapter/persistence/FieldOfLawSpecificationIntegrationTest.java
@@ -13,9 +13,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 class FieldOfLawSpecificationIntegrationTest {
 
   @Autowired
