957b2fcf4c566385cab5051ef86413949687e3e4
lucas <l.solcher@gmail.com>
2025-07-16 15:22:20 +0200

    Add env controller test

---
+++ backend/src/test/java/de/bund/digitalservice/ris/adm_vwv/adapter/api/EnvironmentControllerTest.java
@@ -0,0 +1,40 @@
+package de.bund.digitalservice.ris.adm_vwv.adapter.api;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SecurelessControllerTest(EnvironmentController.class)
+@TestPropertySource(
+  properties = {
+    "frontend.auth.clientId=test-client",
+    "frontend.auth.realm=test-realm",
+    "frontend.auth.url=http://test.url",
+  }
+)
+class EnvironmentControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Nested
+  class getEnvironment {
+
+    @Test
+    void itReturnsTheConfigurationOfTheEnvironment() throws Exception {
+      // When / Then
+      mockMvc
+        .perform(get("/environment"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("authClientId").value("test-client"))
+        .andExpect(jsonPath("authRealm").value("test-realm"))
+        .andExpect(jsonPath("authUrl").value("http://test.url"));
+    }
+  }
+}
