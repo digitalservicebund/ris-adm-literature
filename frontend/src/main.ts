e7c8f5d097ca2b8b33f46a41da1b6464ab225d24
lucas <l.solcher@gmail.com>
2025-07-18 14:55:51 +0200

    Disable sentry for now

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,7 +4,6 @@ import PrimeVue from 'primevue/config'
 import { RisUiTheme, RisUiLocale } from '@digitalservicebund/ris-ui/primevue'
 import '@digitalservicebund/ris-ui/fonts.css'
 import App from './App.vue'
-import * as Sentry from '@sentry/vue'
 import '@/styles/global.css'
 import router from '@/router.ts'
 import { ToastService } from 'primevue'
@@ -16,20 +15,6 @@ try {
   const env = await getEnv()
 
   const app = createApp(App)
-
-  // env.PROD does not mean the "prod" environment, but should be true for all hosted
-  // environments (as opposed to local development), cf: https://vite.dev/guide/env-and-mode
-  // if required locally, we can build and run the app from "/dist" - that one is PROD, too.
-  /*
-  if (import.meta.env.PROD) {
-    Sentry.init({
-      app,
-      environment: window.location.host,
-      dsn: 'https://7c56d29d5dd2c9bd48fc72a8edaffe57@o1248831.ingest.us.sentry.io/4508482613084160',
-      integrations: [],
-    })
-  }
-*/
 
   // Configure authentication
   const auth = useAuthentication()
