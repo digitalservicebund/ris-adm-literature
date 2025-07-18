bfb1fec60f91d4cad7e25699aa010ff3b14ae2f0
lucas <l.solcher@gmail.com>
2025-07-18 15:06:41 +0200

    Enable sentry, update cors

--- backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/config/SecurityConfiguration.java
+++ backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/config/SecurityConfiguration.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -19,6 +20,7 @@ import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
 import org.springframework.security.web.servlet.util.matcher.PathPatternRequestMatcher;
 
 /**
@@ -58,10 +60,37 @@ public class SecurityConfiguration {
       )
       .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
       .csrf(AbstractHttpConfigurer::disable)
-      .cors(Customizer.withDefaults())
       .sessionManagement(sessionManagement ->
         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+      )
+      .headers(httpSecurityHeadersConfigurer ->
+        httpSecurityHeadersConfigurer
+          .contentSecurityPolicy(contentSecurityPolicyConfig ->
+            contentSecurityPolicyConfig.policyDirectives(
+              "default-src 'self'; img-src 'self' data:; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-eval'; connect-src 'self' *.sentry.io data:"
+            )
+          )
+          .contentTypeOptions(_ -> {})
+          .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
+          .referrerPolicy(referrerPolicyConfig ->
+            referrerPolicyConfig.policy(
+              ReferrerPolicyHeaderWriter.ReferrerPolicy.STRICT_ORIGIN_WHEN_CROSS_ORIGIN
+            )
+          )
+          .permissionsPolicyHeader(permissionsPolicyConfig ->
+            permissionsPolicyConfig.policy(
+              "accelerometer=(), ambient-light-sensor=(), autoplay=(), battery=(), camera=(), cross-origin-isolated=(), " +
+              "display-capture=(), document-domain=(), encrypted-media=(), execution-while-not-rendered=(), " +
+              "execution-while-out-of-viewport=(), fullscreen=(), geolocation=(), gyroscope=(), keyboard-map=(), " +
+              "magnetometer=(), microphone=(), midi=(), navigation-override=(), payment=(), picture-in-picture=(), " +
+              "publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(), usb=(), web-share=(), xr-spatial-tracking=(), " +
+              "clipboard-read=(self), clipboard-write=(self), gamepad=(), speaker-selection=(), conversion-measurement=(), " +
+              "focus-without-user-activation=(self), hid=(), idle-detection=(), interest-cohort=(), serial=(), sync-script=(), " +
+              "trust-token-redemption=(), window-placement=(), vertical-scroll=(self)"
+            )
+          )
       );
+
     return http.build();
   }
 
