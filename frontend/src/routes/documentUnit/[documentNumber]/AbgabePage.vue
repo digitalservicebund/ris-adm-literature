1cbe344a49e112e3726d6fc7a5f8cdc7f95def04
Xavier Tremel <xavier.tremel@digitalservice.bund.de>
2025-08-01 10:19:38 +0200

    Add sanity check component

--- frontend/src/routes/documentUnit/[documentNumber]/AbgabePage.vue
+++ frontend/src/routes/documentUnit/[documentNumber]/AbgabePage.vue
@@ -3,6 +3,7 @@ import { useRouter } from 'vue-router'
 import Button from 'primevue/button'
 import IconCheck from '~icons/material-symbols/check'
 import TitleElement from '@/components/TitleElement.vue'
+import SanityCheck from '@/components/publication/SanityCheck.vue'
 const router = useRouter()
 </script>
 
@@ -10,6 +11,7 @@ const router = useRouter()
   <div class="flex w-full flex-1 grow flex-col p-24">
     <div aria-label="Abgabe" class="flex flex-col gap-24 bg-white p-24">
       <TitleElement>Abgabe</TitleElement>
+      <SanityCheck />
       <div class="flex flex-row">
         <Button
           :disabled="false"
