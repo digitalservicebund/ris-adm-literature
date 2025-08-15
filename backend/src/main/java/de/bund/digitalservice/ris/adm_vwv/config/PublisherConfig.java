d413d6ae7537d81cdd981cf64df5b9be0a5641db
lucas <l.solcher@gmail.com>
2025-08-15 11:30:21 +0200

    Update javadoc

--- backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/config/PublisherConfig.java
+++ backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/config/PublisherConfig.java
@@ -38,7 +38,10 @@ public class PublisherConfig {
    * publish requests and delegate them to the appropriate concrete publisher instance
    * based on the {@code targetPublisher} specified in the {@link PublishPort.Options}.
    *
-   * @param allPublishers A list of all other beans that implement the {@link PublishPort} interface, automatically injected by Spring.
+   * @param allPublishers A list of all other beans that implement the {@link PublishPort} interface,
+   *                      automatically injected by Spring. These publisher beans are defined as a bean like
+   *                     {@link PublisherConfig#privateBsgPublisher} where each is configured with a specific S3 client
+   *                      and bucket name.
    * @return A single {@link PublishPort} instance that handles the routing logic.
    */
   @Bean
