5ab83d87f453dcb86990aa193e0fd839bb0ee76c
Hannes <hannes.michalek@digitalservice.bund.de>
2025-09-19 13:45:08 +0200

    Pin netty-codec and netty-codec-http, exclude from awssdk:s3 (#1292)

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -81,7 +81,15 @@ dependencies {
   implementation("io.sentry:sentry-spring-boot-starter-jakarta:$sentryVersion")
   implementation("io.sentry:sentry-logback:$sentryVersion")
   implementation(platform("software.amazon.awssdk:bom:$awsVersion"))
-  implementation("software.amazon.awssdk:s3")
+
+  implementation("software.amazon.awssdk:s3") {
+    // CVE-2025-58056
+    exclude("io.netty", "netty-codec-http")
+    // CVE CVE-2025-58057
+    exclude("io.netty", "netty-codec")
+  }
+  implementation("io.netty:netty-codec:4.1.125.Final")
+  implementation("io.netty:netty-codec-http:4.1.125.Final")
   implementation("org.jsoup:jsoup:$jsoupVersion")
   compileOnly("org.projectlombok:lombok")
   testAndDevelopmentOnly("org.springframework.boot:spring-boot-docker-compose")
