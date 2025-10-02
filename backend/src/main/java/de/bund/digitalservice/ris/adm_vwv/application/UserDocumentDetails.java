9dcc25d23111fc893456686761df11323797245d
lucas <l.solcher@gmail.com>
2025-10-02 09:38:54 +0200

    Add mock adm user for tests

--- backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/UserDocumentDetails.java
+++ backend/src/main/java/de/bund/digitalservice/ris/adm_vwv/application/UserDocumentDetails.java
@@ -8,7 +8,7 @@ import java.io.Serializable;
  * Holds a user's assigned documentation office and document type.
  * <p>
  * This gets created from the user's JWT roles during login and is used as
- * the main user object in Spring Security.
+ * the main user object.
  *
  * @param office The user's documentation office.
  * @param type   The type of document the user works with.
