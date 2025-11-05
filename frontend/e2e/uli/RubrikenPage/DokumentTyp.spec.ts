eab0e7587dc195ec48eff1f0f3f05743f7698cee
andreas-deazevedo <andreas.deazevedo@digitalservice.bund.de>
2025-11-05 14:26:29 +0100

    RISDEV-9866 Fix filtering of document types

--- frontend/e2e/uli/RubrikenPage/DokumentTyp.spec.ts
+++ frontend/e2e/uli/RubrikenPage/DokumentTyp.spec.ts
@@ -3,7 +3,7 @@ import { expect, test } from '@playwright/test'
 test.describe('ULI Rubriken - DokumentTyp', () => {
   test(
     'DokumentTyp is a mandatory field and its value persists after saving and reloading',
-    { tag: ['@RISDEV-9366'] },
+    { tag: ['@RISDEV-9866'] },
     async ({ page }) => {
       // when
       await page.goto('/')
@@ -11,24 +11,24 @@ test.describe('ULI Rubriken - DokumentTyp', () => {
       await page.waitForURL(/dokumentationseinheit/)
 
       // then
-      await expect(page.getByText('DokumentTyp *')).toBeVisible()
+      await expect(page.getByText('Dokumenttyp *')).toBeVisible()
 
       // when
-      const input = page.getByRole('combobox', { name: 'DokumentTyp' })
-      await input.fill('VR')
+      const input = page.getByRole('combobox', { name: 'Dokumenttyp' })
+      await input.fill('Auf')
       // then
-      await expect(page.getByText('Verwaltungsregelung')).toBeVisible()
+      await expect(page.getByText('Aufsatz')).toBeVisible()
 
       // when
-      await page.getByText('Verwaltungsregelung').click()
+      await page.getByText('Aufsatz').click()
       // then
-      await expect(page.getByRole('option', { name: 'VR' }).getByLabel('VR')).toBeVisible()
+      await expect(page.getByRole('option', { name: 'Auf' }).getByLabel('Auf')).toBeVisible()
 
       // when
-      await input.fill('VV')
-      await page.getByText('Verwaltungsvorschrift').click()
+      await input.fill('Kon')
+      await page.getByText('Kongressvortrag').click()
       // then
-      await expect(page.getByRole('option', { name: 'VV' }).getByLabel('VV')).toBeVisible()
+      await expect(page.getByRole('option', { name: 'Kon' }).getByLabel('Kon')).toBeVisible()
 
       // when
       await page.getByText('Speichern').click()
@@ -40,25 +40,25 @@ test.describe('ULI Rubriken - DokumentTyp', () => {
       await page.reload()
 
       // then
-      await expect(page.getByText('VR')).toBeVisible()
-      await expect(page.getByText('VV')).toBeVisible()
+      await expect(page.getByText('Auf')).toBeVisible()
+      await expect(page.getByText('Kon')).toBeVisible()
     },
   )
 
   test(
     'DokumentTyp: an existing value can be deleted',
-    { tag: ['@RISDEV-9366'] },
+    { tag: ['@RISDEV-9866'] },
     async ({ page }) => {
       // given
       await page.goto('/')
       await page.getByText('Neue Dokumentationseinheit').click()
       await page.waitForURL(/dokumentationseinheit/)
-      const input = page.getByRole('combobox', { name: 'DokumentTyp' })
-      await input.fill('VR')
+      const input = page.getByRole('combobox', { name: 'Dokumenttyp' })
+      await input.fill('Ebs')
       await page.getByText('Verwaltungsregelung').click()
       await page.getByText('Speichern').click()
       await page.reload()
-      await expect(page.getByText('VR')).toBeVisible()
+      await expect(page.getByText('Ebs')).toBeVisible()
 
       // when
       // eslint-disable-next-line playwright/no-raw-locators
@@ -67,7 +67,7 @@ test.describe('ULI Rubriken - DokumentTyp', () => {
       await removeIcon.click()
 
       // then
-      await expect(page.getByText('VR')).toBeHidden()
+      await expect(page.getByText('Ebs')).toBeHidden()
     },
   )
 })
