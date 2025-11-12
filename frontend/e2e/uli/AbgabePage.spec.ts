4a682a28302de9d3834cf620b4b69c73f0fa5dd3
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2025-11-12 11:45:52 +0100

    Squashed commit of the following:
    commit 09b0d68b451b83051926a3abb9adb5e84c2d8685
    Author: Hannes Michalek <hannes.michalek@digitalservice.bund.de>
    Date:   Wed Nov 12 11:43:04 2025 +0100
    
        Handle error 500 from the backend
    
    commit ea104e48f74f6039216633e576fd1e9512e84270
    Author: Xavier Tremel <xavier.tremel@digitalservice.bund.de>
    Date:   Wed Nov 12 09:59:47 2025 +0100
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:frontend/e2e/uli/AbgabePage.spec.ts
    
    commit fd9afc27f660b72ece45a3b123fa95fffc5dac39
    Author: Hannes Michalek <hannes.michalek@digitalservice.bund.de>
    Date:   Wed Nov 12 09:46:04 2025 +0100
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:frontend/e2e/uli/AbgabePage.spec.ts

--- frontend/e2e/uli/AbgabePage.spec.ts
+++ frontend/e2e/uli/AbgabePage.spec.ts
@@ -3,7 +3,7 @@ import { expect, test } from '@playwright/test'
 test.describe('ULI AbgabePage', () => {
   test(
     'Should enter mandatory data, validate fine and publish successfully',
-    { tag: ['@RISDEV-9375'] },
+    { tag: ['@RISDEV-9375', '@RISDEV-9374'] },
     async ({ page }) => {
       // given
       await page.goto('/')
@@ -28,6 +28,61 @@ test.describe('ULI AbgabePage', () => {
       await page.getByRole('button', { name: 'Zur Veröffentlichung freigeben' }).click()
       // then
       await expect(page.getByText('Freigabe ist abgeschlossen.')).toBeVisible()
+    },
+  )
+
+  test(
+    'Should show validation error when mandatory fields are missing',
+    { tag: ['@RISDEV-9374'] },
+    async ({ page }) => {
+      // given
+      await page.goto('/')
+      await page.getByText('Neue Dokumentationseinheit').click()
+
+      // when
+      await page.getByText('Abgabe').click()
+      // then
+      await expect(page.getByText('Folgende Pflichtfelder sind nicht befüllt:')).toBeVisible()
+      await expect(page.getByText('Dokumenttyp')).toBeVisible()
+      await expect(page.getByText('Veröffentlichungsjahr')).toBeVisible()
+      await expect(page.getByText('Hauptsachtitel / Dokumentarischer Titel')).toBeVisible()
+      await expect(
+        page.getByRole('button', { name: 'Zur Veröffentlichung freigeben' }),
+      ).toBeDisabled()
+    },
+  )
+
+  test(
+    'Should show a publication error on backend error 500',
+    { tag: ['@RISDEV-9375'] },
+    async ({ page }) => {
+      // given
+      await page.goto('/')
+      await page.getByText('Neue Dokumentationseinheit').click()
+      const input = page.getByRole('combobox', { name: 'Dokumenttyp' })
+      await input.fill('Auf')
+      await page.getByText('Aufsatz').click()
+      await page
+        .getByRole('textbox', { name: 'Hauptsachtitel', exact: true })
+        .fill(
+          'Dieser Test simuliert einen Fehler im Backend; unabhängig davon, was wir hier eingeben.',
+        )
+      await page.getByRole('textbox', { name: 'Veröffentlichungsjahr' }).fill('2025')
+      await page.getByText('Speichern').click()
+      await page.getByText('Abgabe').click()
+
+      // mock an error 500 response on publishing
+      await page.route('*/**/api/literature/documentation-units/*/publish', async (route) => {
+        const response = await route.fetch()
+        await route.fulfill({ status: 500, response })
+      })
+
+      // when
+      await page.getByRole('button', { name: 'Zur Veröffentlichung freigeben' }).click()
+      // then
+      await expect(
+        page.getByText('Die Freigabe ist aus technischen Gründen nicht durchgeführt worden.'),
+      ).toBeVisible()
     },
   )
 })
