32178d847e7d2cc82ad732f332bdac434d044b6b
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2025-11-12 08:15:31 +0100

    Expect successful validation and publishing.

--- frontend/e2e/uli/RubrikenPage/Publishing.spec.ts
+++ frontend/e2e/uli/RubrikenPage/Publishing.spec.ts
@@ -2,8 +2,8 @@ import { expect, test } from '@playwright/test'
 
 test.describe('ULI Publishing', () => {
   test(
-    'DokumentTyp is a mandatory field and its value persists after saving and reloading',
-    { tag: ['@RISDEV-9866'] },
+    'Should enter mandatory data, validate fine and publish successfully',
+    { tag: ['@RISDEV-9375'] },
     async ({ page }) => {
       // when
       await page.goto('/')
@@ -14,13 +14,19 @@ test.describe('ULI Publishing', () => {
       await page.getByText('Aufsatz').click()
       await input.fill('Kon')
       await page.getByText('Kongressvortrag').click()
+      await page
+        .getByRole('textbox', { name: 'Hauptsachtitel', exact: true })
+        .fill('Die unendliche Verhandlung 123äöüß$%&')
+      await page.getByRole('textbox', { name: 'Veröffentlichungsjahr' }).fill('2025')
       await page.getByText('Speichern').click()
 
-      // goto publishing
+      // when
+      await page.getByText('Abgabe').click()
+      page.getByText('Alle Pflichtfelder sind korrekt ausgefüllt.')
+      await page.getByRole('button', { name: 'Zur Veröffentlichung freigeben' }).click()
 
-      // publish
-
-      // expect confirmation
+      // then
+      await page.getByText('Freigabe ist abgeschlossen.').click()
     },
   )
 })
