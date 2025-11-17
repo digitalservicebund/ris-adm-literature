53bf294ce05b0465898f22d1e097bcf623d0479e
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2025-11-17 14:46:03 +0100

    Fill in first couple of fields

--- frontend/e2e/adm/AbgabePage.spec.ts
+++ frontend/e2e/adm/AbgabePage.spec.ts
@@ -103,5 +103,15 @@ test.describe('ADM AbgabePage', () => {
     await page.getByText('ABc | Die Beispieler').click()
     await page.getByRole('textbox', { name: 'Zitatstelle' }).fill('2024, Seite 24')
     await page.getByText('Übernehmen').click()
+
+    await page.getByRole('link', { name: 'Rubriken' }).click()
+    await page.getByText('Amtl. Langüberschrift *').fill('This is my Langüberschrift')
+    await page.getByRole('group', { name: 'Zitierdatum' }).locator('input').fill('15.01.2025')
+
+    await page.getByRole('combobox', { name: 'Normgeber' }).click()
+    await page.getByText('Erstes Organ').click()
+    await page.getByRole('combobox', { name: 'Region' }).fill('AA')
+    await page.getByText('AA').click()
+    await page.getByRole('button', { name: 'Normgeber übernehmen', exact: true }).click()
   })
 })
