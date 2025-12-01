f2d7a1caf5396d9711882e4c60b1bdbc668c3221
andreas-deazevedo <andreas.deazevedo@digitalservice.bund.de>
2025-12-01 10:06:35 +0100

    RISDEV-10229 Log configuration

--- backend/src/main/java/de/bund/digitalservice/ris/adm_literature/config/multischema/DataSourceConfig.java
+++ backend/src/main/java/de/bund/digitalservice/ris/adm_literature/config/multischema/DataSourceConfig.java
@@ -1,9 +1,8 @@
 package de.bund.digitalservice.ris.adm_literature.config.multischema;
 
 import javax.sql.DataSource;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceProperties;
 import org.springframework.context.annotation.Bean;
@@ -17,13 +16,10 @@ import org.springframework.core.env.Environment;
  */
 @Configuration
 @Slf4j
+@RequiredArgsConstructor
 public class DataSourceConfig {
 
-  @Value("${spring.datasource.adm.database.username}")
-  private String user;
-
-  @Autowired
-  private Environment environment;
+  private final Environment environment;
 
   /**
    * Loads the data source properties for the 'adm' schema.
@@ -57,7 +53,6 @@ public class DataSourceConfig {
     DataSourceProperties dataSourceProperties = admDataSourceProperties();
     log.info("Adm database url: '{}'", dataSourceProperties.getUrl());
     log.info("Adm database user: '{}'", dataSourceProperties.getUsername());
-    log.info("Adm database user from config: '{}'", user);
     log.info(environment.getProperty("spring.datasource.adm.username"));
     log.info(environment.getProperty("spring.datasource.literature.database.user"));
     log.info(environment.getProperty("s3.bucket.adm.public.bucket-name"));
