a59e915945d6e6df5d8eb449e6c40d76243e7d5c
Xavier Tremel <xavier.tremel@digitalservice.bund.de>
2025-12-12 17:13:21 +0100

    Add tests

--- frontend/src/components/aktivzitierung/adm/AktvizitierungAdmItem.spec.ts
+++ frontend/src/components/aktivzitierung/adm/AktvizitierungAdmItem.spec.ts
@@ -1,21 +1,50 @@
-import { render, screen } from '@testing-library/vue'
 import { describe, it, expect } from 'vitest'
+import { render, screen } from '@testing-library/vue'
 import type { AktivzitierungAdm } from '@/domain/AktivzitierungAdm.ts'
 import AktivzitierungAdmItem from './AktivzitierungAdmItem.vue'
 
 describe('AktivzitierungAdmItem', () => {
-  it('renders metaSummary correctly when documentNumber is set', () => {
+  it('renders only documentNumber when citationType is missing', () => {
     const aktivzitierung: AktivzitierungAdm = {
       id: '1',
-      uuid: undefined,
-      documentNumber: 'DOC123',
+      documentNumber: 'DOC-123',
+      citationType: undefined,
     }
 
     render(AktivzitierungAdmItem, {
       props: { aktivzitierung },
     })
 
-    const div = screen.getByText('DOC123')
-    expect(div).toBeInTheDocument()
+    expect(screen.getByText('DOC-123')).toBeInTheDocument()
+    expect(screen.queryByText('Citation A')).not.toBeInTheDocument()
+  })
+
+  it('renders only citationType when documentNumber is missing', () => {
+    const aktivzitierung: AktivzitierungAdm = {
+      id: '1',
+      documentNumber: undefined,
+      citationType: 'Citation A',
+    }
+
+    render(AktivzitierungAdmItem, {
+      props: { aktivzitierung },
+    })
+
+    expect(screen.getByText('Citation A')).toBeInTheDocument()
+    expect(screen.queryByText('DOC-123')).not.toBeInTheDocument()
+  })
+
+  it('renders both documentNumber and citationType', () => {
+    const aktivzitierung: AktivzitierungAdm = {
+      id: '1',
+      documentNumber: 'DOC-123',
+      citationType: 'Citation A',
+    }
+
+    render(AktivzitierungAdmItem, {
+      props: { aktivzitierung },
+    })
+
+    expect(screen.getByText('DOC-123, Citation A')).toBeInTheDocument()
   })
 })
