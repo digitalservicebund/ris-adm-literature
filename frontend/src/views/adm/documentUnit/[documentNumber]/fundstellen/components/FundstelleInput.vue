27c1c817a3726ff74dfe4d9a8ca63c761a5c23fd
Xavier Tremel <xavier.tremel@digitalservice.bund.de>
2025-12-15 13:32:51 +0100

    Add dropdown folder

--- frontend/src/views/adm/documentUnit/[documentNumber]/fundstellen/components/FundstelleInput.vue
+++ frontend/src/views/adm/documentUnit/[documentNumber]/fundstellen/components/FundstelleInput.vue
@@ -5,7 +5,7 @@ import { type Fundstelle, type Periodikum } from '@/domain/fundstelle'
 import InputText from 'primevue/inputtext'
 import Button from 'primevue/button'
 import { useValidationStore } from '@/composables/useValidationStore'
-import PeriodikumDropDown from '@/components/PeriodikumDropDown.vue'
+import PeriodikumDropDown from '@/components/dropdown/PeriodikumDropDown.vue'
 
 const props = defineProps<{
   fundstelle?: Fundstelle
--- frontend/src/views/adm/documentUnit/[documentNumber]/rubriken/components/normgeber/NormgeberInput.vue
+++ frontend/src/views/adm/documentUnit/[documentNumber]/rubriken/components/normgeber/NormgeberInput.vue
@@ -6,7 +6,7 @@ import InputText from 'primevue/inputtext'
 import Button from 'primevue/button'
 import { useValidationStore } from '@/composables/useValidationStore'
 import { useAdmDocUnitStore } from '@/stores/admDocumentUnitStore'
-import InstitutionDropDown from '@/components/InstitutionDropDown.vue'
+import InstitutionDropDown from '@/components/dropdown/InstitutionDropDown.vue'
 import RegionDropDown from './RegionDropDown.vue'
 
 const props = defineProps<{
