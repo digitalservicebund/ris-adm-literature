5d39cd2e2d051bbac907d5dced25fce6999d04e4
andreas-deazevedo <andreas.deazevedo@digitalservice.bund.de>
2025-12-22 09:09:42 +0100

    RISDEV-10416 Add Javadoc

--- backend/src/main/java/de/bund/digitalservice/ris/adm_literature/documentation_unit/literature/aktivzitierung/AdmAktivzitierungController.java
+++ backend/src/main/java/de/bund/digitalservice/ris/adm_literature/documentation_unit/literature/aktivzitierung/AdmAktivzitierungController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Rest controller for searching adm references (documentation units) to be used as 'Aktivzitierungen'.
+ */
 @RestController
 @RequiredArgsConstructor
 public class AdmAktivzitierungController {
