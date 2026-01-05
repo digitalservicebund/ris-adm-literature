1925aaff599bf63648d57cdf1f0dbf761254d321
andreas-deazevedo <andreas.deazevedo@digitalservice.bund.de>
2026-01-05 09:42:54 +0100

    Use generics

--- backend/src/main/java/de/bund/digitalservice/ris/adm_literature/documentation_unit/converter/LdmlToObjectConverterService.java
+++ backend/src/main/java/de/bund/digitalservice/ris/adm_literature/documentation_unit/converter/LdmlToObjectConverterService.java
@@ -27,9 +27,9 @@ public class LdmlToObjectConverterService {
    * @param clazz The class of the returned business model
    * @return Business model representation of given documentation unit's xml
    */
-  public DocumentationUnitContent convertToBusinessModel(
+  public <T extends DocumentationUnitContent> T convertToBusinessModel(
     @Nonnull DocumentationUnit documentationUnit,
-    Class<? extends DocumentationUnitContent> clazz
+    Class<T> clazz
   ) {
     log.debug("Finding converter strategy for content type: {}", clazz.getSimpleName());
 
@@ -44,6 +44,7 @@ public class LdmlToObjectConverterService {
       );
 
     log.debug("Using strategy: {}", strategy.getClass().getSimpleName());
-    return strategy.convertToBusinessModel(documentationUnit);
+    //noinspection unchecked
+    return (T) strategy.convertToBusinessModel(documentationUnit);
   }
 }
