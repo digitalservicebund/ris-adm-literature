a5a1946bfee006361490b20b8b7ca97ff048c357
andreas-deazevedo <andreas.deazevedo@digitalservice.bund.de>
2026-02-16 09:54:21 +0100

    RISDEV-10990 Fix creation of ref views

--- backend/src/main/resources/data-references.sql
+++ backend/src/main/resources/data-references.sql
@@ -1,4 +1,4 @@
-DROP VIEW ref_view_adm;
+DROP VIEW IF EXISTS ref_view_adm;
 CREATE OR REPLACE VIEW ref_view_adm AS
 SELECT du.id,
        du.document_number,
@@ -18,7 +18,7 @@ FROM adm.documentation_unit_index dui
          join adm.documentation_unit du on dui.documentation_unit_id = du.id
 WHERE du.xml is not null;
 
-DROP VIEW ref_view_active_reference_adm_sli;
+DROP VIEW IF EXISTS  ref_view_active_reference_adm_sli;
 CREATE OR REPLACE VIEW ref_view_active_reference_adm_sli AS
 SELECT adm_ars.id,
        adm_ars.source_documentation_unit_id,
@@ -32,7 +32,7 @@ SELECT adm_ars.id,
        adm_ars.published_at
 FROM adm.active_reference_sli adm_ars;
 
-DROP VIEW ref_view_active_reference_adm_uli;
+DROP VIEW IF EXISTS  ref_view_active_reference_adm_uli;
 CREATE OR REPLACE VIEW ref_view_active_reference_adm_uli AS
 SELECT adm_aru.id,
        adm_aru.source_documentation_unit_id,
@@ -45,7 +45,7 @@ SELECT adm_aru.id,
        adm_aru.published_at
 FROM adm.active_reference_uli adm_aru;
 
-DROP VIEW ref_view_active_reference_adm_caselaw;
+DROP VIEW IF EXISTS  ref_view_active_reference_adm_caselaw;
 CREATE OR REPLACE VIEW ref_view_active_reference_adm_caselaw AS
 SELECT adm_arc.id,
        adm_arc.source_documentation_unit_id,
@@ -60,7 +60,7 @@ SELECT adm_arc.id,
        adm_arc.published_at
 FROM adm.active_reference_caselaw adm_arc;
 
-DROP VIEW ref_view_literature;
+DROP VIEW IF EXISTS  ref_view_literature;
 CREATE OR REPLACE VIEW ref_view_literature AS
 SELECT du.id,
        du.document_number,
@@ -76,7 +76,7 @@ FROM literature.documentation_unit_index dui
          join literature.documentation_unit du on dui.documentation_unit_id = du.id
 WHERE du.xml is not null;
 
-DROP VIEW ref_view_active_reference_sli_adm;
+DROP VIEW IF EXISTS  ref_view_active_reference_sli_adm;
 CREATE OR REPLACE VIEW ref_view_active_reference_sli_adm AS
 SELECT literature_ar_adm.id,
        literature_ar_adm.source_documentation_unit_id,
@@ -95,7 +95,7 @@ FROM literature.active_reference_adm literature_ar_adm
          join literature.documentation_unit source_du on literature_ar_adm.source_documentation_unit_id = source_du.id
 WHERE source_du.documentation_unit_type = 'LITERATUR_SELBSTAENDIG';
 
-DROP VIEW ref_view_active_reference_uli_adm;
+DROP VIEW IF EXISTS  ref_view_active_reference_uli_adm;
 CREATE OR REPLACE VIEW ref_view_active_reference_uli_adm AS
 SELECT literature_ar_adm.id,
        literature_ar_adm.source_documentation_unit_id,
